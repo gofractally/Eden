bb2b4d4a70a3ccd5c6ab471ef3e77302852186ec
Spark Plug <sparkplug0025@gmail.com>
2021-04-18 07:42:28 -0400

    cleanup

--- packages/webapp/src/members/utils.ts
+++ packages/webapp/src/members/utils.ts
@@ -6,10 +6,8 @@ export const accountTo32BitHash = (account: string): number[] =>
     hash.sha256().update(account).digest().slice(0, 4);
 
 export const pkFromAccountInstant = (account: string): string => {
-    const accountHash = hash.sha256().update(account).digest().slice(0, 4);
-
     const serialBuffer = new eosjsSerialize.SerialBuffer();
-    serialBuffer.pushArray(accountHash);
+    serialBuffer.pushArray(accountTo32BitHash(account));
     serialBuffer.pushUint32(Date.now());
 
     const bytes = serialBuffer.getUint8Array(8);
