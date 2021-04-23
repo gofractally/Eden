0a72d0b539a27a65512effc993f8378863734f68
Spark Plug <sparkplug0025@gmail.com>
2021-04-23 06:30:24 -0400

    clear all update

--- contracts/eden/src/actions/genesis.cpp
+++ contracts/eden/src/actions/genesis.cpp
@@ -10,6 +10,7 @@ namespace eden
       require_auth(get_self());
       members{get_self()}.clear_all();
       inductions{get_self()}.clear_all();
+      get_global_singleton(get_self()).remove();
    }
 
    void eden::genesis(std::string community,
