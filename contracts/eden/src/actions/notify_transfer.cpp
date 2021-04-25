3b0af76dfdf705aab15d4da0ead5593a79878aac
SparkPlug0025 <79721020+sparkplug0025@users.noreply.github.com>
2021-04-25 11:05:06 -0400

    Update notify_transfer.cpp

--- contracts/eden/src/actions/notify_transfer.cpp
+++ contracts/eden/src/actions/notify_transfer.cpp
@@ -13,8 +13,6 @@ namespace eden
 
       globals globals{get_self()};
       globals.check_active();
-
-      eosio::check(to == get_self(), "only accepting transfers to us");
       eosio::check(quantity.symbol == globals.default_token(),
                    "token must be a valid " + globals.default_token().to_string());
 
