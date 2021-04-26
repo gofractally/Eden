139f6287afab68a431e06f0686fd56709d1e38a1
Steven Watanabe <steven@clarionos.com>
2021-04-26 10:14:34 -0400

    Convert members to variant

--- contracts/eden/include/utils.hpp
+++ contracts/eden/include/utils.hpp
@@ -9,4 +9,13 @@ namespace eden
    {
       return uint128_t{a.value} << 64 | b.value;
    }
+
+#define EDEN_FORWARD_FUNCTION(var, fun) auto fun() const { return std::visit([](auto& value){ return value.fun(); }, var); }
+#define EDEN_FORWARD_FUNCTIONS(var, ...) EOSIO_MAP_REUSE_ARG0(EDEN_FORWARD_FUNCTION, var, __VA_ARGS__)
+
+#define EDEN_FORWARD_MEMBER(var, member)                                \
+   decltype(auto) member() { return std::visit([](auto& value) -> decltype(auto) { return (value.member); }, var); } \
+   decltype(auto) member() const { return std::visit([](auto& value) -> decltype(auto) { return (value.member); }, var); }
+#define EDEN_FORWARD_MEMBERS(var, ...) EOSIO_MAP_REUSE_ARG0(EDEN_FORWARD_MEMBER, var, __VA_ARGS__)
+
 }  // namespace eden
