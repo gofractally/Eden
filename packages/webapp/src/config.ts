b11e2e7daa8d25ade4e959013ec844bff719b389
Spark Plug <sparkplug0025@gmail.com>
2021-04-27 09:06:06 -0400

    changes ipfs gateway

--- packages/webapp/src/config.ts
+++ packages/webapp/src/config.ts
@@ -38,7 +38,7 @@ EDEN_CONTRACT_ACCOUNT="${process.env.NEXT_PUBLIC_EDEN_CONTRACT_ACCOUNT}"
 APP_MINIMUM_DONATION_AMOUNT="${process.env.NEXT_PUBLIC_APP_MINIMUM_DONATION_AMOUNT}"
 `);
 
-export const ipfsBaseUrl = "https://ipfs.pink.gg/ipfs";
+export const ipfsBaseUrl = "https://ipfs.io/ipfs"; //"https://ipfs.pink.gg/ipfs";
 
 export const shortAppName = process.env.NEXT_PUBLIC_APP_SHORT_NAME;
 export const appName = process.env.NEXT_PUBLIC_APP_NAME;
