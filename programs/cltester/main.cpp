a715209d34a97e2681d6b4dc3b0dd73b904bcaef
Todd Fleming <tbfleming@gmail.com>
2021-04-29 13:43:08 -0400

    Rename eden-tester to cltester

--- programs/cltester/main.cpp
+++ programs/cltester/main.cpp
@@ -1223,7 +1223,7 @@ static void run(const char* wasm, const std::vector<std::string>& args)
 }
 
 const char usage[] =
-    "usage: eden-tester [-h or --help] [-v or --verbose] file.wasm [args for wasm]\n";
+    "usage: cltester [-h or --help] [-v or --verbose] file.wasm [args for wasm]\n";
 
 int main(int argc, char* argv[])
 {
