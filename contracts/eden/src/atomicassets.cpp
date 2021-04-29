b06f0804dfc8b6813ce31067b80eaa1c0e7ceb7b
Spark Plug <sparkplug0025@gmail.com>
2021-04-29 07:51:23 -0400

    Revert "fixes schema img and video to ipfs"
    This reverts commit 09ff24cba4aeb33c356415125ad620cc267d07dd.

--- contracts/eden/src/atomicassets.cpp
+++ contracts/eden/src/atomicassets.cpp
@@ -89,8 +89,9 @@ namespace eden::atomicassets
              .send(self, collection, true, std::vector{self}, std::vector{self}, market_fee, attrs);
       }
 
-      std::vector<format> schema{{"account", "string"}, {"name", "string"},   {"img", "ipfs"},
-                                 {"bio", "string"},     {"social", "string"}, {"video", "ipfs"}};
+      std::vector<format> schema{{"account", "string"}, {"name", "string"},
+                                 {"img", "string"},     {"bio", "string"},
+                                 {"social", "string"},  {"video", "string"}};
       ::atomicassets::schemas_t schemas(contract, collection.value);
       if (auto pos = schemas.find(schema_name.value); pos != schemas.end())
       {
