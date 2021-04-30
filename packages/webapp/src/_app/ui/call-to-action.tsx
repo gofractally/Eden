985140ee4037d746061085185cea947117cb73a5
Brandon Fancher <brandonfancher@gmail.com>
2021-04-30 13:57:59 -0400

    Make button in CallToAction optional.

--- packages/webapp/src/_app/ui/call-to-action.tsx
+++ packages/webapp/src/_app/ui/call-to-action.tsx
@@ -3,7 +3,7 @@ import { ActionButton, ActionButtonSize, Card } from "_app";
 interface Props {
     href?: string;
     onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    buttonLabel: string;
+    buttonLabel?: string;
     children: React.ReactNode;
 }
 
@@ -16,19 +16,27 @@ export const CallToAction = ({
     <Card>
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
-                <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
-                    <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">
-                        {children}
-                    </h1>
-                    <ActionButton
-                        href={href}
-                        onClick={onClick}
-                        size={ActionButtonSize.L}
-                        className="flex-shrink-0 mt-10 sm:mt-0"
-                    >
-                        {buttonLabel}
-                    </ActionButton>
-                </div>
+                {href || onClick ? (
+                    <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
+                        <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">
+                            {children}
+                        </h1>
+                        <ActionButton
+                            href={href}
+                            onClick={onClick}
+                            size={ActionButtonSize.L}
+                            className="flex-shrink-0 mt-10 sm:mt-0"
+                        >
+                            {buttonLabel || "Go"}
+                        </ActionButton>
+                    </div>
+                ) : (
+                    <div className="lg:w-4/5 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
+                        <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">
+                            {children}
+                        </h1>
+                    </div>
+                )}
             </div>
         </section>
     </Card>
