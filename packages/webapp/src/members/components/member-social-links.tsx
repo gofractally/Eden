5bbf6a435f447195a3beb1f0b868d521b02583b8
Spark Plug <sparkplug0025@gmail.com>
2021-05-04 08:12:29 -0400

    add all social icons and overall improvements

--- packages/webapp/src/members/components/member-social-links.tsx
+++ packages/webapp/src/members/components/member-social-links.tsx
@@ -1,10 +1,13 @@
+import { blockExplorerBaseUrl } from "config";
 import {
-    FaAirbnb,
-    FaBitcoin,
     FaBlogger,
+    FaFacebook,
+    FaLinkedin,
     FaTelegram,
     FaTwitter,
 } from "react-icons/fa";
+import { IoChatbubblesOutline } from "react-icons/io5";
+import { GenIcon } from "react-icons/lib";
 
 import { SocialButton } from "_app";
 import { MemberData } from "../interfaces";
@@ -13,45 +16,91 @@ interface Props {
     member: MemberData;
 }
 
+const urlify = (address: string) => {
+    let domainBeginIndex = 0;
+
+    const protocolIndex = address.indexOf("//");
+    if (protocolIndex > -1 && protocolIndex <= 6) {
+        domainBeginIndex = protocolIndex + 2;
+    }
+
+    return `//${address.substring(domainBeginIndex)}`;
+};
+
 export const MemberSocialLinks = ({ member }: Props) => (
-    <nav className="space-y-2 flex flex-col">
-        <SocialButton
-            handle={member.account}
-            icon={FaBitcoin}
-            color="black"
-            href={`https://bloks.io/account/${member.account}`}
-        />
-        {member.socialHandles.telegram && (
+    <div className="flex flex-col sm:flex-row">
+        <div className="space-y-2">
             <SocialButton
-                handle={member.socialHandles.telegram}
-                icon={FaTelegram}
-                color="indigo"
-                href={`https://t.me/${member.socialHandles.telegram}`}
+                handle={member.account}
+                icon={EosCommunityIcon}
+                color="black"
+                href={`${blockExplorerBaseUrl}/account/${member.account}`}
             />
-        )}
-        {member.socialHandles.twitter && (
-            <SocialButton
-                handle={member.socialHandles.twitter}
-                icon={FaTwitter}
-                color="blue"
-                href={`https://twitter.com/${member.socialHandles.twitter}`}
-            />
-        )}
-        {member.socialHandles.eosCommunity && (
-            <SocialButton
-                handle={member.socialHandles.eosCommunity}
-                icon={FaAirbnb}
-                color="red"
-                href={`https://eoscommunity.org/u/${member.socialHandles.eosCommunity}`}
-            />
-        )}
-        {member.socialHandles.blog && (
-            <SocialButton
-                handle="Blog"
-                icon={FaBlogger}
-                color="yellow"
-                href={member.socialHandles.blog}
-            />
-        )}
-    </nav>
+            {member.socialHandles.eosCommunity && (
+                <SocialButton
+                    handle={member.socialHandles.eosCommunity}
+                    icon={IoChatbubblesOutline}
+                    color="gray"
+                    href={`https://eoscommunity.org/u/${member.socialHandles.eosCommunity}`}
+                />
+            )}
+            {member.socialHandles.blog && (
+                <SocialButton
+                    handle="Blog"
+                    icon={FaBlogger}
+                    color="yellow"
+                    href={urlify(member.socialHandles.blog)}
+                />
+            )}
+        </div>
+        <div className="mt-2 sm:mt-0 sm:ml-4 space-y-2">
+            {member.socialHandles.twitter && (
+                <SocialButton
+                    handle={member.socialHandles.twitter}
+                    icon={FaTwitter}
+                    color="blue"
+                    href={`https://twitter.com/${member.socialHandles.twitter}`}
+                />
+            )}
+            {member.socialHandles.telegram && (
+                <SocialButton
+                    handle={member.socialHandles.telegram}
+                    icon={FaTelegram}
+                    color="blue"
+                    href={`https://t.me/${member.socialHandles.telegram}`}
+                />
+            )}
+            {member.socialHandles.linkedin && (
+                <SocialButton
+                    handle={member.socialHandles.linkedin}
+                    icon={FaLinkedin}
+                    color="blue"
+                    href={`https://www.linkedin.com/in/${member.socialHandles.linkedin}`}
+                />
+            )}
+            {member.socialHandles.facebook && (
+                <SocialButton
+                    handle={member.socialHandles.facebook}
+                    icon={FaFacebook}
+                    color="indigo"
+                    href={`https://facebook.com/${member.socialHandles.facebook}`}
+                />
+            )}
+        </div>
+    </div>
 );
+
+const EosCommunityIcon = (props: any) =>
+    GenIcon({
+        tag: "svg",
+        attr: { viewBox: "0 0 32.2 48" },
+        child: [
+            {
+                tag: "path",
+                attr: {
+                    d:
+                        "M16.1 0L4.8 15.5 0 38.3 16.1 48l16.1-9.7-4.8-22.9L16.1 0zM7.4 15.9L16.1 4l8.7 11.9L16.1 42 7.4 15.9zM26 19.8l3.6 17.4-11.8 7.1L26 19.8zM2.6 37.2l3.6-17.4 8.2 24.5-11.8-7.1z",
+                },
+            },
+        ],
+    } as any)(props);
