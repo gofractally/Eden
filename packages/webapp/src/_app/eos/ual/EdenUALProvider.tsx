a9dd1e461e1fa796352a1457a977bf0b1ced1626
Spark Plug <sparkplug0025@gmail.com>
2021-05-05 08:09:57 -0400

    adds scatter and ledger

--- packages/webapp/src/_app/eos/ual/EdenUALProvider.tsx
+++ packages/webapp/src/_app/eos/ual/EdenUALProvider.tsx
@@ -1,13 +1,13 @@
 import { UALProvider } from "ual-reactjs-renderer";
 
 import { appName, chainConfig } from "config";
-import { anchor } from "./config";
+import { anchor, scatter, ledger } from "./config";
 
 export const EdenUALProvider: React.FC = ({ children }) => {
     return (
         <UALProvider
             chains={[chainConfig]}
-            authenticators={[anchor]}
+            authenticators={[anchor, scatter, ledger]}
             appName={appName}
         >
             {children}
