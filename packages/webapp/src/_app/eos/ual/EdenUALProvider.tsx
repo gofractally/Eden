e1eb6244118836132b191626dba2785331e0c783
SparkPlug0025 <79721020+sparkplug0025@users.noreply.github.com>
2021-05-06 08:13:25 -0400

    Fixes EdenUALProvider SSR x Client Component re-rendering (#129)

--- packages/webapp/src/_app/eos/ual/EdenUALProvider.tsx
+++ packages/webapp/src/_app/eos/ual/EdenUALProvider.tsx
@@ -1,18 +1,29 @@
-import { UALProvider } from "ual-reactjs-renderer";
+import { useEffect, useState } from "react";
 
 import { appName, chainConfig } from "config";
 import { anchor, scatter, ledger } from "./config";
 
 export const EdenUALProvider: React.FC = ({ children }) => {
-    return (
-        <UALProvider
-            chains={[chainConfig]}
-            authenticators={[anchor, scatter, ledger]}
-            appName={appName}
-        >
-            {children}
-        </UALProvider>
-    );
+    const [hasMounted, setHasMounted] = useState(false);
+
+    useEffect(() => {
+        setHasMounted(true);
+    }, []);
+
+    if (!hasMounted) {
+        return <>{children}</>;
+    } else {
+        const UALProvider = require("ual-reactjs-renderer").UALProvider;
+        return (
+            <UALProvider
+                chains={[chainConfig]}
+                authenticators={[anchor, scatter, ledger]}
+                appName={appName}
+            >
+                {children}
+            </UALProvider>
+        );
+    }
 };
 
 export default EdenUALProvider;
