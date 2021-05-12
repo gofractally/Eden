e14bf206c0f641aa9da80f3571ce705d87423e26
Brandon Fancher <brandonfancher@gmail.com>
2021-05-12 10:22:09 -0400

    Get An Invite CTA journey graphic should display from Invitee perspective.

--- packages/webapp/src/inductions/components/get-an-invite-cta.tsx
+++ packages/webapp/src/inductions/components/get-an-invite-cta.tsx
@@ -7,7 +7,7 @@ import {
 
 export const GetAnInviteCTA = () => {
     return (
-        <InductionJourneyContainer role={InductionRole.INVITER} step={1}>
+        <InductionJourneyContainer role={InductionRole.INVITEE} step={1}>
             <>
                 <p className="mb-10 text-2xl font-medium title-font text-gray-900">
                     Ready to join Eden? The membership process begins with an
