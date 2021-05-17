8a4f857435a414eb4ce30575bde3cc1250bae874
Spark Plug <sparkplug0025@gmail.com>
2021-05-17 08:55:19 -0400

    api refactor

--- packages/webapp/src/_api/error-handlers.ts
+++ packages/webapp/src/_api/error-handlers.ts
@@ -1,7 +1,31 @@
 import { NextApiResponse } from "next";
 
-export const handleErrors = (
-    res: NextApiResponse,
-    errors: string[],
-    statusCode = 400
-) => res.status(statusCode).json({ errors });
+export class ServerError extends Error {
+    constructor(public status: number, public errors: string[]) {
+        super(`Server Error: ${status} ${errors[0]}`);
+        this.name = "ServerError";
+    }
+}
+export class BadRequestError extends ServerError {
+    constructor(errors: string[]) {
+        super(400, errors);
+        this.name = "BadRequestError";
+    }
+}
+
+export class InternalServerError extends ServerError {
+    constructor(errors: string[]) {
+        super(500, errors);
+        this.name = "InternalServerError";
+    }
+}
+
+export const handleErrors = (res: NextApiResponse, error: Error) => {
+    if (error instanceof ServerError) {
+        return res.status(error.status).json({ errors: error.errors });
+    } else {
+        return res
+            .status(500)
+            .json({ error: error.message || "Unknown error" });
+    }
+};
