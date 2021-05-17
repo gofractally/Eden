c30790347a7ce8c7d1791edd66befb02e601daa5
Brandon Fancher <brandonfancher@gmail.com>
2021-05-17 11:43:21 -0400

    Adjust links in footer and home.

--- packages/webapp/src/_app/ui/footer.tsx
+++ packages/webapp/src/_app/ui/footer.tsx
@@ -1,62 +1,85 @@
 import { Link } from "_app/ui";
 
-interface Props {
-    
-}
-
-export const Footer = ({ }: Props) => (
+export const Footer = () => (
     <footer className="border-t text-gray-600 body-font border-gray-200 bg-white">
         <div className="container px-4 py-8 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-          <div className="w-64 m flex-shrink-0 mx-auto text-center md:text-left">
-            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <span className="text-xl">We are...</span>
-            </a>
-            <p className="mt-2 text-sm text-gray-500">A community working to maximize the power and independence of its members, thereby securing life, liberty, property, and justice for all.</p>
-          </div>
-          <div className="flex-grow flex flex-wrap md:pl-20 -mb-8 md:mt-0 mt-10 md:text-left text-center">
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">RESOURCES</h2>
-              <nav className="list-none mb-10">
-                <li><Link href="/members">The Community</Link></li>
-                <li><Link href="/induction">Membership Dashboard</Link></li>
-                <li>
-                  <Link
-                    href="https://www.notion.so/edenos/Eden-d1446453c66c4919b110dfdce20dc56f"
-                    target="_blank"
-                    isExternal
-                  >Eden Public Wiki</Link>
-                </li>
-                <li>
-                  <Link
-                      href="https://github.com/eoscommunity/Eden"
-                      target="_blank"
-                      isExternal
-                  >EdenOS Github Repo</Link>
-                </li>
-              </nav>
+            <div className="w-64 m flex-shrink-0 mx-auto text-center md:text-left">
+                <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                    <span className="text-xl">We are...</span>
+                </a>
+                <p className="mt-2 text-sm text-gray-500">
+                    A community working to maximize the power and independence
+                    of its members, thereby securing life, liberty, property,
+                    and justice for all.
+                </p>
             </div>
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">RESOURCES</h2>
-              <nav className="list-none mb-10">
-                <li><Link href="/members">The Community</Link></li>
-                <li><Link href="/induction">Membership Dashboard</Link></li>
-                <li>
-                  <Link
-                    href="https://www.notion.so/edenos/Eden-d1446453c66c4919b110dfdce20dc56f"
-                    target="_blank"
-                    isExternal
-                  >Eden Public Wiki</Link>
-                </li>
-                <li>
-                  <Link
-                      href="https://github.com/eoscommunity/Eden"
-                      target="_blank"
-                      isExternal
-                  >EdenOS Github Repo</Link>
-                </li>
-              </nav>
+            <div className="flex-grow flex flex-wrap md:pl-20 -mb-8 md:mt-0 mt-10 md:text-left text-center">
+                <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                        EDEN
+                    </h2>
+                    <nav className="list-none mb-10">
+                        <li>
+                            <Link href="/members">The Community</Link>
+                        </li>
+                        <li>
+                            <Link href="/induction">Membership Dashboard</Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="https://www.notion.so/edenos/Getting-an-Invite-2d38947d5be94dcb84dfa1ae48894802"
+                                isExternal
+                                target="_blank"
+                            >
+                                Get an Invite
+                            </Link>
+                        </li>
+                    </nav>
+                </div>
+                <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                        RESOURCES
+                    </h2>
+                    <nav className="list-none mb-10">
+                        <li>
+                            <Link
+                                href="http://eden.eoscommunity.org"
+                                isExternal
+                                target="_blank"
+                            >
+                                Eden on EOS
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="https://www.notion.so/edenos/Eden-d1446453c66c4919b110dfdce20dc56f"
+                                target="_blank"
+                                isExternal
+                            >
+                                Eden Public Wiki
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="https://www.notion.so/edenos/EdenOS-Roadmap-7d75dbcf386c436c9c1738b7a3eea8f2"
+                                target="_blank"
+                                isExternal
+                            >
+                                EdenOS Roadmap
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="https://github.com/eoscommunity/Eden"
+                                target="_blank"
+                                isExternal
+                            >
+                                EdenOS Github Repo
+                            </Link>
+                        </li>
+                    </nav>
+                </div>
             </div>
-          </div>
         </div>
     </footer>
 );
--- packages/webapp/src/pages/index.tsx
+++ packages/webapp/src/pages/index.tsx
@@ -1,49 +1,40 @@
-import { Card, Link, RawLayout, Text, Footer } from "_app";
+import { Card, Link, SingleColLayout, Text } from "_app";
 
 export const Index = () => (
-    <RawLayout>
-        <div className="grid grid-cols-3 gap-4">
-            <Card title="Welcome to Eden" className="col-span-3 xl:col-span-2">
-                <div className="space-y-3 text-gray-800">
-                    <Text>
-                        Eden is a community working to maximize the power and
-                        independence of its members, thereby securing life,
-                        liberty, property, and justice for all.
-                    </Text>
-                    <Text>
-                        A team of people can be more powerful than the sum of
-                        its members, but all teams need a means to reach
-                        consensus, or they will fall apart. Unfortunately,
-                        traditional democratic processes end up empowering
-                        politicians and disempowering the people who
-                        participate. EdenOS is a revolutionary new democratic
-                        process that protects and enhances the independence and
-                        power of those who join. When you join the Eden
-                        community, you gain access to a group of people working
-                        together to empower you and your family to make a bigger
-                        impact in the world.
-                    </Text>
-                    <Text>
-                        To learn more about Eden and how you can get involved,
-                        visit{" "}
-                        <Link
-                            href="http://eden.eoscommunity.org"
-                            target="_blank"
-                            isExternal
-                        >
-                            eden.eoscommunity.org
-                        </Link>
-                        .
-                    </Text>
-                </div>
-            </Card>
-            <Card
-                title=""
-                className="col-span-3 xl:col-span-1"
-            >
-            </Card>
-        </div>
-    </RawLayout>
+    <SingleColLayout>
+        <Card title="Welcome to Eden" className="col-span-3 xl:col-span-2">
+            <div className="space-y-3 text-gray-800">
+                <Text>
+                    Eden is a community working to maximize the power and
+                    independence of its members, thereby securing life, liberty,
+                    property, and justice for all.
+                </Text>
+                <Text>
+                    A team of people can be more powerful than the sum of its
+                    members, but all teams need a means to reach consensus, or
+                    they will fall apart. Unfortunately, traditional democratic
+                    processes end up empowering politicians and disempowering
+                    the people who participate. EdenOS is a revolutionary new
+                    democratic process that protects and enhances the
+                    independence and power of those who join. When you join the
+                    Eden community, you gain access to a group of people working
+                    together to empower you and your family to make a bigger
+                    impact in the world.
+                </Text>
+                <Text>
+                    To learn more about Eden and how you can get involved, visit{" "}
+                    <Link
+                        href="http://eden.eoscommunity.org"
+                        target="_blank"
+                        isExternal
+                    >
+                        eden.eoscommunity.org
+                    </Link>
+                    .
+                </Text>
+            </div>
+        </Card>
+    </SingleColLayout>
 );
 
 export default Index;
