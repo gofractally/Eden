5080dc9150b64029311b8d811e17e6f5ac96b047
Spark Plug <sparkplug0025@gmail.com>
2021-05-19 09:18:01 -0400

    handle eos errors

--- packages/webapp/src/_app/utils/error-handler.tsx
+++ packages/webapp/src/_app/utils/error-handler.tsx
@@ -1,5 +1,4 @@
 import toast from "react-hot-toast";
-import { Toast } from "react-hot-toast/dist/core/types";
 import { HelpLink } from "_app/ui";
 
 export const onError = (error: Error, title = "") => {
@@ -8,54 +7,59 @@ export const onError = (error: Error, title = "") => {
     let message = error.toString();
     message = message.replace("Error: assertion failure with message:", "");
 
-    let toaster = toast.error;
-
-    const eosResourceless = message.indexOf("transaction net usage") >= 0;
-    if (eosResourceless) {
-        toaster = toast;
-        title = "Insufficient Resources";
+    const insufficientResources =
+        message.indexOf("transaction net usage") >= 0 ||
+        message.indexOf("billed CPU time") >= 0;
+    if (insufficientResources) {
+        return eosResourcesError(message);
     }
 
-    toaster(
-        (t) => (
-            <div>
-                {title && (
-                    <>
-                        <strong>{title}</strong>
-                        <br />
-                    </>
-                )}
-                {eosResourceless ? (
-                    <EosResourceless t={t} message={message} />
-                ) : (
-                    message
-                )}
-            </div>
-        ),
-        {
-            duration: eosResourceless ? 999_999_999 : undefined,
-        }
+    toast.error(
+        <div>
+            {title && (
+                <>
+                    <strong>{title}</strong>
+                    <br />
+                </>
+            )}
+            {message}
+        </div>
     );
 };
 
-const EosResourceless = ({ t, message }: { t: Toast; message: string }) => (
-    <div className="mt-2">
-        <div className="flex items-center mb-1 space-x-1">
+const eosResourcesError = (message: string) => {
+    toast(
+        (t) => (
             <div>
-                <p>
-                    It seems like your EOS Account does not have necessary
-                    resources to execute your transaction.
-                </p>
+                <>
+                    <strong>Insufficient Resources</strong>
+                    <br />
+                </>
+                <div className="mt-2">
+                    <div className="flex items-center mb-1 space-x-1">
+                        <div>
+                            <p>
+                                It seems like your EOS Account does not have
+                                necessary resources to execute your transaction.
+                            </p>
 
-                <p className="mt-2 text-xs text-red-500">{message}</p>
+                            <p className="mt-2 text-xs text-red-500">
+                                {message}
+                            </p>
+                        </div>
+                        <HelpLink href="https://www.notion.so/edenos/EdenOS-c0c42c7c426f407fadb215a017b54eb9" />
+                    </div>
+                    <button
+                        className="text-sm underline hover:text-black mt-2"
+                        onClick={() => toast.dismiss(t.id)}
+                    >
+                        Dismiss
+                    </button>
+                </div>
             </div>
-            <HelpLink href="https://www.notion.so/edenos/EdenOS-c0c42c7c426f407fadb215a017b54eb9" />
-        </div>
-        <button
-            className="text-sm underline hover:text-black mt-2"
-            onClick={() => toast.dismiss(t.id)}
-        >
-            Dismiss
-        </button>
-    </div>
-);
+        ),
+        {
+            duration: 999_999_999,
+        }
+    );
+};
