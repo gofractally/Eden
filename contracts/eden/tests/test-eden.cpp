21ede6e064898bf2bf3c82b84cf881ff217a34f7
Steven Watanabe <steven@clarionos.com>
2021-05-21 09:57:13 -0400

    Check issuing NFTs when cancelling a genesis member's induction.

--- contracts/eden/tests/test-eden.cpp
+++ contracts/eden/tests/test-eden.cpp
@@ -386,31 +386,31 @@ TEST_CASE("genesis replacement")
    CHECK(get_eden_membership("pip"_n).status() == eden::member_status::pending_membership);
    CHECK(get_eden_membership("egeon"_n).status() == eden::member_status::pending_membership);
 
+   t.alice.act<actions::inductprofil>(1, alice_profile);
+   t.alice.act<token::actions::transfer>("alice"_n, "eden.gm"_n, s2a("100.0000 EOS"), "memo");
+   t.alice.act<actions::inductdonate>("alice"_n, 1, s2a("10.0000 EOS"));
+
    t.eden_gm.act<actions::inductcancel>("eden.gm"_n, 2);
    t.eden_gm.act<actions::addtogenesis>("bertie"_n);
 
-   CHECK(get_eden_membership("alice"_n).status() == eden::member_status::pending_membership);
+   CHECK(get_eden_membership("alice"_n).status() == eden::member_status::active_member);
    CHECK(get_eden_membership("egeon"_n).status() == eden::member_status::pending_membership);
    CHECK(get_eden_membership("bertie"_n).status() == eden::member_status::pending_membership);
 
-   CHECK(members("eden.gm"_n).stats().active_members == 0);
-   CHECK(members("eden.gm"_n).stats().pending_members == 3);
-   CHECK(get_table_size<eden::induction_table_type>() == 3);
-   CHECK(get_table_size<eden::endorsement_table_type>() == 6);
+   CHECK(members("eden.gm"_n).stats().active_members == 1);
+   CHECK(members("eden.gm"_n).stats().pending_members == 2);
+   CHECK(get_table_size<eden::induction_table_type>() == 2);
+   CHECK(get_table_size<eden::endorsement_table_type>() == 4);
 
-   t.alice.act<actions::inductprofil>(1, alice_profile);
    t.egeon.act<actions::inductprofil>(3, egeon_profile);
    t.bertie.act<actions::inductprofil>(4, bertie_profile);
 
-   t.alice.act<token::actions::transfer>("alice"_n, "eden.gm"_n, s2a("100.0000 EOS"), "memo");
    t.egeon.act<token::actions::transfer>("egeon"_n, "eden.gm"_n, s2a("10.0000 EOS"), "memo");
    t.bertie.act<token::actions::transfer>("bertie"_n, "eden.gm"_n, s2a("10.0000 EOS"), "memo");
 
-   t.alice.act<actions::inductdonate>("alice"_n, 1, s2a("10.0000 EOS"));
    t.egeon.act<actions::inductdonate>("egeon"_n, 3, s2a("10.0000 EOS"));
    t.bertie.act<actions::inductdonate>("bertie"_n, 4, s2a("10.0000 EOS"));
 
-   CHECK(get_eden_membership("alice"_n).status() == eden::member_status::active_member);
    CHECK(get_eden_membership("egeon"_n).status() == eden::member_status::active_member);
    CHECK(get_eden_membership("bertie"_n).status() == eden::member_status::active_member);
 
@@ -421,6 +421,25 @@ TEST_CASE("genesis replacement")
    CHECK(get_table_size<eden::induction_table_type>() == 0);
    CHECK(get_table_size<eden::endorsement_table_type>() == 0);
    CHECK(get_table_size<eden::member_table_type>() == 3);
+
+   // Verify that all members have the same set of NFTs
+   std::vector<int32_t> expected_assets;
+   for (auto member : {"alice"_n, "egeon"_n, "bertie"_n})
+   {
+      expected_assets.push_back(get_eden_membership(member).nft_template_id());
+   }
+   std::sort(expected_assets.begin(), expected_assets.end());
+   for (auto member : {"alice"_n, "egeon"_n, "bertie"_n, "atomicmarket"_n})
+   {
+      INFO(member.to_string() << "'s assets");
+      auto assets = eden::atomicassets::assets_by_owner(eden::atomic_assets_account, member);
+      std::sort(assets.begin(), assets.end());
+      CHECK(assets == expected_assets);
+   }
+   // The removed member should only have NFTs issued before his removal
+   CHECK(eden::atomicassets::assets_by_owner(eden::atomic_assets_account, "pip"_n) ==
+         std::vector<int32_t>{
+             static_cast<int32_t>(get_eden_membership("alice"_n).nft_template_id())});
 }
 
 TEST_CASE("induction")
