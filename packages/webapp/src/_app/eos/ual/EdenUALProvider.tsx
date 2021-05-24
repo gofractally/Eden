32b91ccd5475eca7e9ae82eacca43e83fe08e95b
Spark Plug <sparkplug0025@gmail.com>
2021-05-24 08:25:18 -0400

    Add Dynamic Wallets Config Vars

--- packages/webapp/src/_app/eos/ual/EdenUALProvider.tsx
+++ packages/webapp/src/_app/eos/ual/EdenUALProvider.tsx
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
 
-import { appName, chainConfig } from "config";
+import { appName, availableWallets, chainConfig } from "config";
 import { anchor, scatter, ledger } from "./config";
+
+const authenticators: any[] = [];
+if (availableWallets.indexOf("ANCHOR") >= 0) {
+    authenticators.push(anchor);
+}
+if (availableWallets.indexOf("SCATTER") >= 0) {
+    authenticators.push(scatter);
+}
+if (availableWallets.indexOf("LEDGER") >= 0) {
+    authenticators.push(ledger);
+}
 
 export const EdenUALProvider: React.FC = ({ children }) => {
     const [hasMounted, setHasMounted] = useState(false);
@@ -17,7 +28,7 @@ export const EdenUALProvider: React.FC = ({ children }) => {
         return (
             <UALProvider
                 chains={[chainConfig]}
-                authenticators={[anchor, scatter, ledger]}
+                authenticators={authenticators}
                 appName={appName}
             >
                 {children}
