4c9cff1628b93d10d5b66a4b52a67b8bac74b0d8
Spark Plug <sparkplug0025@gmail.com>
2021-06-16 08:29:43 -0400

    test create meeting

--- packages/webapp/src/pages/api/redirect/zoom.ts
+++ packages/webapp/src/pages/api/redirect/zoom.ts
@@ -1,12 +1,8 @@
-// http://localhost:3000/api/redirect/zoom?code=n5fFXS8S68_9fmhzhg5T9CBi8dPqn6LMQ
-
 import { zoom } from "config";
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as z from "zod";
 
 import { BadRequestError, handleErrors } from "_api/error-handlers";
-// import { ipfsPostSchema } from "_api/schemas";
-// import { ipfsUploadHandler } from "_api/handlers";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     switch (req.method) {
@@ -52,5 +48,31 @@ const handleGetZoomOAuth = async (
     );
     const zoomResponseData = await zoomResponse.json();
 
-    return res.status(200).json({ data, zoomResponseData });
+    const testMeeting = await testCreateMeeting(zoomResponseData.access_token);
+
+    return res.status(200).json({ data, zoomResponseData, testMeeting });
+};
+
+const testCreateMeeting = async (accessToken: string) => {
+    const body = {
+        topic: "Sparky Test Eden Election #5",
+        duration: 10,
+        start_time: "2025-06-15T13:35:00Z",
+        settings: {
+            join_before_host: true,
+            jbh_time: 0,
+            auto_recording: "local",
+        },
+    };
+
+    const response = await fetch(`https://api.zoom.us/v2/users/me/meetings`, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+
+    return response.json();
 };
