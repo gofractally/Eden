64da0ca960ac5aee563123090a4a5b2a78fcaf26
Mike Manfredi <mike@clarionos.com>
2021-07-01 17:45:18 -0500

    reordering one file

--- packages/webapp/src/members/helpers/social-links.ts
+++ packages/webapp/src/members/helpers/social-links.ts
@@ -8,6 +8,10 @@ export const stripLeadingAtSymbol = (
     return str;
 };
 
+export const getStringAfterTrailingSlash = (
+    str: string | undefined
+): string | undefined => str && str.slice(str.lastIndexOf("/") + 1);
+
 export const getTwitterHandle = (
     profileTwitter: string | undefined
 ): string | undefined => stripLeadingAtSymbol(profileTwitter);
@@ -15,10 +19,6 @@ export const getTwitterHandle = (
 export const getTelegramHandle = (
     profileTelegram: string | undefined
 ): string | undefined => stripLeadingAtSymbol(profileTelegram);
-
-export const getStringAfterTrailingSlash = (
-    str: string | undefined
-): string | undefined => str && str.slice(str.lastIndexOf("/") + 1);
 
 export const getFacebookHandle = (
     profileFacebook: string | undefined
