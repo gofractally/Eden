d54afcfb92d01c0b1ebb495075bb41fc3af49263
Mike Manfredi <mike@clarionos.com>
2021-07-01 17:58:55 -0500

    reordering again to put most consumable function at the top

--- packages/webapp/src/members/helpers/social-links.ts
+++ packages/webapp/src/members/helpers/social-links.ts
@@ -1,17 +1,3 @@
-export const stripLeadingAtSymbol = (
-    str: string | undefined
-): string | undefined => {
-    if (str && str.startsWith("@")) {
-        return str.slice(1);
-    }
-
-    return str;
-};
-
-export const getStringAfterTrailingSlash = (
-    str: string | undefined
-): string | undefined => str && str.slice(str.lastIndexOf("/") + 1);
-
 export const getTwitterHandle = (
     profileTwitter: string | undefined
 ): string | undefined => stripLeadingAtSymbol(profileTwitter);
@@ -26,3 +12,17 @@ export const getFacebookHandle = (
 export const getLinkedinHandle = (
     profileTelegram: string | undefined
 ): string | undefined => getStringAfterTrailingSlash(profileTelegram);
+
+export const stripLeadingAtSymbol = (
+    str: string | undefined
+): string | undefined => {
+    if (str && str.startsWith("@")) {
+        return str.slice(1);
+    }
+
+    return str;
+};
+
+export const getStringAfterTrailingSlash = (
+    str: string | undefined
+): string | undefined => str && str.slice(str.lastIndexOf("/") + 1);
