bb675efa46188e1f66ca36210e238a9521151f56
Brandon Fancher <brandonfancher@gmail.com>
2021-07-07 18:39:52 -0400

    Add (styled) react-modal component. Add xs breakpoint.

--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     purge: ["./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
+        screens: {
+            xs: "475px",
+            ...defaultTheme.screens,
+        },
     },
     variants: {
         extend: {
