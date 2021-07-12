6a1461c0fdcabd4f0d1a7a4957fabeb114e31140
Brandon Fancher <brandonfancher@gmail.com>
2021-07-12 16:58:54 -0400

    Move more layout into FluidLayout component.

--- packages/webapp/src/_app/layouts/fluid-layout.tsx
+++ packages/webapp/src/_app/layouts/fluid-layout.tsx
@@ -5,15 +5,26 @@ import { HeaderNav, Footer } from "../ui";
 interface Props {
     title?: string;
     children: React.ReactNode;
+    hideBorders?: boolean;
 }
 
-export const FluidLayout = ({ children, title }: Props) => (
+export const FluidLayout = ({ children, title, hideBorders }: Props) => (
     <div className="flex flex-col min-h-screen">
         <HeaderNav />
         <Head>
             <title>{title && `${title} | `} Eden</title>
         </Head>
-        <main className="xl:container xl:mx-auto flex-grow">{children}</main>
+        <main className="flex-grow">
+            <div className="max-w-screen-xl mx-auto">
+                <div
+                    className={
+                        hideBorders ? "lg:mx-5" : "lg:mx-5 border border-t-0"
+                    }
+                >
+                    {children}
+                </div>
+            </div>
+        </main>
         <Footer />
     </div>
 );
