f3eb5dafda52c6d51c960f0730a05ec912918457
Todd Fleming <tbfleming@gmail.com>
2021-07-13 22:20:43 -0400

    filtering

--- programs/demo-micro-chain/demo-micro-chain.cpp
+++ programs/demo-micro-chain/demo-micro-chain.cpp
@@ -1,7 +1,15 @@
+#include <boost/multi_index/mem_fun.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index_container.hpp>
+#include <chainbase/chainbase.hpp>
 #include <clchain/crypto.hpp>
 #include <clchain/eden_chain.hpp>
 #include <clchain/graphql.hpp>
+#include <eden.hpp>
 #include <eosio/to_bin.hpp>
+
+using namespace eosio::literals;
 
 extern "C" void __wasm_call_ctors();
 [[clang::export_name("initialize")]] void initialize()
@@ -17,6 +25,186 @@ extern "C" void __wasm_call_ctors();
 [[clang::export_name("free_memory")]] void free_memory(void* p)
 {
    free(p);
+}
+
+namespace boost
+{
+   BOOST_NORETURN void throw_exception(std::exception const& e)
+   {
+      eosio::detail::assert_or_throw(e.what());
+   }
+   BOOST_NORETURN void throw_exception(std::exception const& e, boost::source_location const& loc)
+   {
+      eosio::detail::assert_or_throw(e.what());
+   }
+}  // namespace boost
+
+enum tables
+{
+   induction_table
+};
+
+struct induction
+{
+   uint64_t id;
+   eosio::name inviter;
+   eosio::name invitee;
+   std::vector<eosio::name> witnesses;
+   eden::new_member_profile profile;
+   std::string video;
+};
+EOSIO_REFLECT(induction, id, inviter, invitee, witnesses, profile, video)
+
+void dump(const induction& ind)
+{
+   printf("%s\n", eosio::format_json(ind).c_str());
+}
+
+struct induction_object : public chainbase::object<induction_table, induction_object>
+{
+   CHAINBASE_DEFAULT_CONSTRUCTOR(induction_object)
+
+   id_type id;
+   induction induction;
+
+   uint64_t pk() const { return induction.id; }
+};
+
+struct by_id;
+struct by_pk;
+
+typedef boost::multi_index_container<
+    induction_object,
+    boost::multi_index::indexed_by<
+        boost::multi_index::ordered_unique<  //
+            boost::multi_index::tag<by_id>,
+            boost::multi_index::
+                member<induction_object, induction_object::id_type, &induction_object::id>>,
+        boost::multi_index::ordered_unique<  //
+            boost::multi_index::tag<by_pk>,
+            boost::multi_index::const_mem_fun<induction_object, uint64_t, &induction_object::pk>>>,
+    chainbase::allocator<induction_object>>
+    induction_index;
+CHAINBASE_SET_INDEX_TYPE(induction_object, induction_index)
+
+struct database
+{
+   chainbase::database db;
+   chainbase::generic_index<induction_index> inductions;
+
+   database() { db.add_index(inductions); }
+};
+database db;
+
+template <typename Tag, typename Index, typename Key, typename F>
+void add_or_modify(Index& index, const Key& key, F&& f)
+{
+   auto& idx = index.template get<Tag>();
+   auto it = idx.find(key);
+   if (it != idx.end())
+      index.modify(*it, f);
+   else
+      index.emplace(f);
+}
+
+template <typename Tag, typename Index, typename Key, typename F>
+void add_or_replace(Index& index, const Key& key, F&& f)
+{
+   auto& idx = index.template get<Tag>();
+   auto it = idx.find(key);
+   if (it != idx.end())
+      index.remove(*it);
+   index.emplace(f);
+}
+
+template <typename Tag, typename Index, typename Key>
+void remove_if_exists(Index& index, const Key& key)
+{
+   auto& idx = index.template get<Tag>();
+   auto it = idx.find(key);
+   if (it != idx.end())
+      index.remove(*it);
+}
+
+void inductinit(uint64_t id,
+                eosio::name inviter,
+                eosio::name invitee,
+                std::vector<eosio::name> witnesses)
+{
+   // TODO: expire records
+   printf("inductinit %llu\n", id);
+   add_or_replace<by_pk>(db.inductions, id, [&](auto& obj) {
+      obj.induction.id = id;
+      obj.induction.inviter = inviter;
+      obj.induction.invitee = invitee;
+      obj.induction.witnesses = witnesses;
+      dump(obj.induction);
+   });
+}
+
+void inductprofil(uint64_t id, eden::new_member_profile profile)
+{
+   printf("inductprofil %llu\n", id);
+   add_or_modify<by_pk>(db.inductions, id, [&](auto& obj) {
+      obj.induction.id = id;  // !!!
+      obj.induction.profile = profile;
+      dump(obj.induction);
+   });
+}
+
+void inductvideo(eosio::name account, uint64_t id, std::string video)
+{
+   printf("inductvideo %016llx\n", id);
+   add_or_modify<by_pk>(db.inductions, id, [&](auto& obj) {
+      obj.induction.id = id;  // !!!
+      obj.induction.video = video;
+      dump(obj.induction);
+   });
+}
+
+void inductcancel(eosio::name account, uint64_t id)
+{
+   printf("inductcancel %016llx\n", id);
+   remove_if_exists<by_pk>(db.inductions, id);
+}
+
+void inductdonate(eosio::name payer, uint64_t id, eosio::asset quantity)
+{
+   printf("inductdonate %016llx\n", id);
+}
+
+template <typename... Args>
+void call(void (*f)(Args...), const std::vector<char>& data)
+{
+   std::tuple<eosio::remove_cvref_t<Args>...> t;
+   eosio::input_stream s(data);
+   // TODO: prevent abort, indicate what failed
+   eosio::from_bin(t, s);
+   std::apply([f](auto&&... args) { f(std::move(args)...); }, t);
+}
+
+// TODO: configurable contract accounts
+void filter_block(const eden_chain::eosio_block& block)
+{
+   for (auto& trx : block.transactions)
+   {
+      for (auto& action : trx.actions)
+      {
+         if (action.firstReceiver == "genesis.eden"_n)
+         {
+            if (action.name == "inductinit"_n)
+               call(inductinit, action.hexData.data);
+            else if (action.name == "inductprofil"_n)
+               call(inductprofil, action.hexData.data);
+            else if (action.name == "inductvideo"_n)
+               call(inductvideo, action.hexData.data);
+            else if (action.name == "inductcancel"_n)
+               call(inductcancel, action.hexData.data);
+            else if (action.name == "inductdonate"_n)
+               call(inductdonate, action.hexData.data);
+         }
+      }
+   }
 }
 
 eden_chain::block_log block_log;
@@ -46,11 +234,13 @@ eden_chain::block_log block_log;
       bi.block = std::move(eden_block);
       bi.id = clchain::sha256(bin.data(), bin.size());
       auto status = block_log.add_block(bi);
+      // TODO: notify chainbase of forks & irreversible
+      filter_block(bi.block.eosio_block);
       // printf("%s block %u %s\n", block_log.status_str[status], bi.block.eosio_block.num,
       //        to_string(bi.block.eosio_block.id).c_str());
    }
-   printf("%d blocks processed, %d blocks now in log\n", (int)eosio_blocks.size(),
-          (int)block_log.blocks.size());
+   // printf("%d blocks processed, %d blocks now in log\n", (int)eosio_blocks.size(),
+   //        (int)block_log.blocks.size());
 }
 
 struct Query
