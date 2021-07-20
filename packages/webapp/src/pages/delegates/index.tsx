2c811156f981ed3b6ec1f614af0af569d58e2ce0
Mike Manfredi <mike@clarionos.com>
2021-07-20 02:30:21 -0500

    My Delegation works

--- packages/webapp/src/pages/delegates/index.tsx
+++ packages/webapp/src/pages/delegates/index.tsx
@@ -12,7 +12,7 @@ import {
     useCurrentMember,
     useUALAccount,
 } from "_app";
-import { MemberData, MemberStats, MemberStatus } from "members";
+import { MemberData } from "members";
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     const queryClient = new QueryClient();
@@ -31,179 +31,141 @@ interface Props {
 const MEMBERS_PAGE_SIZE = 18;
 
 export const DelegatesPage = (props: Props) => {
-    const [ualAccount, ualLogout, ualShowModal] = useUALAccount();
+    const [ualAccount] = useUALAccount();
     const accountName = ualAccount?.accountName;
-    console.info("ualAccount:");
-    console.info(ualAccount);
-    // const loggedInMember = {
-    //     account: "edenmember12",
-    // };
-    // const isLoadingLoggedInMember = false;
-    // const isLoggedInMemberError = false;
-    const {
-        data: currentMember,
-        isLoading: isLoadingLoggedInMember,
-        isError: isLoggedInMemberError,
-    } = useCurrentMember();
+    const { data: currentMember } = useCurrentMember();
 
-    const { isError: isMembersDataFetchError, data: members } = useQuery({
+    const { data: members } = useQuery({
         ...queryMembers(1, MEMBERS_PAGE_SIZE),
         keepPreviousData: true,
     });
 
-    const {
-        isError: isMemberStatsDataFetchError,
-        data: membersStats,
-    } = useQuery({
+    const { data: membersStats } = useQuery({
         ...queryMembersStats,
         keepPreviousData: true,
     });
 
-    const {
-        isError: isLeadRepresentativeDataFetchError,
-        data: lead_representative,
-    } = useQuery({
+    const { data: lead_representative } = useQuery({
         ...queryHeadDelegate,
         keepPreviousData: true,
     });
 
-    const rank = 3;
-    console.info(
-        `determining loggedInMember currentMember[${currentMember}], accountName[${accountName}]`
-    );
-    const loggedInMember = currentMember?.name || accountName;
+    const loggedInMemberName = currentMember?.name || accountName;
+    const loggedInMember: MemberData | undefined = members?.filter(
+        (member) => member.account === loggedInMemberName
+    )[0];
 
-    const getMyDelegation = (
-        members: MemberData[],
-        membersStats: MemberStats
-    ): string[] => {
-        let myDelegates: string[] = [];
-        console.info("getMyDelegates.loggedInMember:");
-        console.info(loggedInMember);
+    const getMyDelegation = (members: MemberData[]): MemberData[] => {
+        let myDelegates: MemberData[] = [];
         if (loggedInMember === undefined) return myDelegates;
         // establish height of tree from electionState
-        const heightOfDelegationTree = membersStats?.ranks.length;
+        // const heightOfDelegationTree = membersStats?.ranks.length;
         // get logged-in user's rank
         // loggedInMember.election_rank
         // loggedInMember.representative
         // get lead rep's account name
-        let m: MemberData = members.filter(
-            (member) => member.account === loggedInMember?.account
-        )[0];
-        // console.info("m:");
-        // console.info(m);
+        let m: MemberData = getMemberRecordFromName(
+            members,
+            loggedInMember?.account
+        );
         while (m && m?.account != lead_representative) {
-            // console.info(
-            //     `while m.account[${m?.account}, m.rep[${m.representative}], lead_representative[${lead_representative}]`
-            // );
-            myDelegates.push(m?.account);
-            m = members.filter(
-                (member) => member.account === m?.representative
-            )[0];
+            myDelegates.push(m);
+            m = getMemberRecordFromName(members, m?.representative!);
         }
-        myDelegates.length && myDelegates.push(lead_representative!);
-        // console.info("myDelegates:");
-        // console.info(myDelegates);
-        // iterate through members and build the tree
-        // console.info("members:");
-        // members.forEach((member) => {
-        //     console.info(member);
-        // });
+        members &&
+            myDelegates.length &&
+            myDelegates.push(
+                getMemberRecordFromName(members, lead_representative!)
+            );
         return myDelegates;
     };
 
-    const delegates: string[] =
+    const getMemberRecordFromName = (
+        members: MemberData[],
+        memberAccount: string
+    ) => members?.filter((member) => member.account === memberAccount)[0];
+
+    const delegates: MemberData[] =
         members && membersStats && loggedInMember
-            ? getMyDelegation(members, membersStats)
+            ? getMyDelegation(members)
             : [];
 
-    console.info(
-        `isLoadingLoggedInMember[${isLoadingLoggedInMember}], isLoggedInMemberError[${isLoggedInMemberError}]`
-    );
-    if (isLoadingLoggedInMember) {
-        return <div>Loading...</div>;
-    } else if (isLoggedInMemberError) {
-        return <div>Error...</div>;
-    } else if (!loggedInMember) {
-        return <div>No one logged in: loggedInMember: {loggedInMember}</div>;
-    } else {
-        console.info(`rendering page.loggedInMember:${loggedInMember}`);
-        return (
-            <RawLayout title="Election">
-                <Text size="sm" className="mb-8">
-                    Note: Data is in square brackets if it's not JSON (to show
-                    if something's undefined)
+    return (
+        <RawLayout title="Election">
+            <Text size="sm" className="mb-8">
+                Note: Data is in square brackets if it's not JSON (to show if
+                something's undefined)
+            </Text>
+            <div>
+                <Text size="lg" className="bg-gray-200">
+                    My Delegation
+                </Text>
+                <Text size="sm" className="mt-4">
+                    {`You [${loggedInMember?.account}] are level (rank) ${loggedInMember?.election_rank} out of ${membersStats?.ranks.length}`}
+                </Text>
+                <Text
+                    size="sm"
+                    className="mt-4"
+                >{`Your Delegates are as follows (You at the bottom; Head Chief at the top):`}</Text>
+                <ul>
+                    {/* {delegates.map((delegate) => (
+                            <li key={delegate.account}> {delegate}</li>
+                        ))} */}
+                    {delegates.reverse().map((delegate) => (
+                        <li key={delegate.account}> {delegate.name}</li>
+                    ))}
+                </ul>
+            </div>
+            <Text size="lg" className="bg-gray-200">
+                -- Raw Table Data --
+            </Text>
+            <div>
+                <Text size="lg" className="mb-4">
+                    Member Stats
+                </Text>
+                <Text size="sm" className="mb-4">
+                    Note: the new field `ranks[]` is{" "}
+                    <span className="font-bold">
+                        not relevant to the frontend
+                    </span>
+                    ; it's a convenience for the smart contract. It's the number
+                    of people at each rank, ranks[ranks.length-1] being 1 for
+                    the Head Chief, ranks[ranks.length-2] being the number of
+                    Chiefs, etc.
                 </Text>
                 <div>
-                    <Text size="lg" className="bg-gray-200">
-                        My Delegation
-                    </Text>
-                    <Text size="sm">
-                        {`You [${loggedInMember}] are rank ${currentMember?.election_rank} out of ${membersStats?.ranks.length}`}
-                    </Text>
-                    <Text size="sm">{`Your Delegates are as follow:`}</Text>
-                    <ul>
-                        {delegates.map((delegate) => (
-                            <li key={delegate}>{delegate}</li>
-                        ))}
-                    </ul>
-
-                    <div>
-                        <pre>{JSON.stringify(membersStats || {}, null, 2)}</pre>
-                    </div>
+                    <pre>{JSON.stringify(membersStats || {}, null, 2)}</pre>
                 </div>
-                <Text size="lg" className="bg-gray-200">
+            </div>
+            <div>
+                <Text size="lg" className="mb-4">
                     -- Raw Table Data --
                 </Text>
+                <Text size="lg" className="bg-gray-200">
+                    Members
+                </Text>
+                <Text size="sm" className="mb-4">
+                    <code>{`enum for status { pending = 0, active }`}</code>
+                </Text>
+                <Text size="sm" className="mb-4">
+                    <code>
+                        {`enum for election_participation_status { no_donation = 0, in_election, not_in_election, recently_inducted }`}
+                    </code>
+                </Text>
                 <div>
-                    <Text size="lg" className="mb-4">
-                        Member Stats
-                    </Text>
-                    <Text size="sm" className="mb-4">
-                        Note: the new field `ranks[]` is{" "}
-                        <span className="font-bold">
-                            not relevant to the frontend
-                        </span>
-                        ; it's a convenience for the smart contract. It's the
-                        number of people at each rank, ranks[ranks.length-1]
-                        being 1 for the Head Chief, ranks[ranks.length-2] being
-                        the number of Chiefs, etc.
-                    </Text>
-                    <div>
-                        <pre>{JSON.stringify(membersStats || {}, null, 2)}</pre>
-                    </div>
+                    <Text size="sm">Sampling a single member for space...</Text>
+                    <pre>
+                        {JSON.stringify(
+                            (members && members.length && members[0]) || {},
+                            null,
+                            2
+                        )}
+                    </pre>
                 </div>
-                <div>
-                    <Text size="lg" className="mb-4">
-                        -- Raw Table Data --
-                    </Text>
-                    <Text size="lg" className="bg-gray-200">
-                        Members
-                    </Text>
-                    <Text size="sm" className="mb-4">
-                        <code>{`enum for status { pending = 0, active }`}</code>
-                    </Text>
-                    <Text size="sm" className="mb-4">
-                        <code>
-                            {`enum for election_participation_status { no_donation = 0, in_election, not_in_election, recently_inducted }`}
-                        </code>
-                    </Text>
-                    <div>
-                        <Text size="sm">
-                            Sampling a single member for space...
-                        </Text>
-                        <pre>
-                            {JSON.stringify(
-                                (members && members.length && members[0]) || {},
-                                null,
-                                2
-                            )}
-                        </pre>
-                    </div>
-                </div>
-            </RawLayout>
-        );
-    }
+            </div>
+        </RawLayout>
+    );
+    // }
 };
 
 export default DelegatesPage;
