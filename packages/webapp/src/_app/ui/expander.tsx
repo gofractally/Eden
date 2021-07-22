e9e285c52c8ddbf74c94d4f83fbd43df4ecb449b
Brandon Fancher <brandonfancher@gmail.com>
2021-07-22 13:30:00 -0400

    Create new Expander component.

---
+++ packages/webapp/src/_app/ui/expander.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { IoChevronDownSharp, IoChevronUpSharp } from "react-icons/io5";
+
+import { Container } from "_app";
+
+interface ExpanderProps {
+    header: React.ReactNode;
+    hideContentDivider?: boolean;
+    startExpanded?: boolean;
+    children: React.ReactNode;
+}
+
+export const Expander = ({
+    header,
+    hideContentDivider,
+    startExpanded = false,
+    children,
+}: ExpanderProps) => {
+    const [isExpanded, setIsExpanded] = useState<boolean>(startExpanded);
+    const containerClass =
+        "flex justify-between items-center group cursor-pointer hover:bg-gray-100 active:bg-gray-200 select-none";
+    const contentDividerClass =
+        !isExpanded || hideContentDivider ? "" : "border-b border-gray-100";
+    return (
+        <div>
+            <Container
+                className={`${containerClass} ${contentDividerClass}`}
+                onClick={() => setIsExpanded(!isExpanded)}
+            >
+                {header}
+                {isExpanded ? (
+                    <IoChevronUpSharp
+                        size={22}
+                        className="text-gray-400 group-hover:text-gray-500 active:text-gray-600"
+                    />
+                ) : (
+                    <IoChevronDownSharp
+                        size={22}
+                        className="text-gray-400 group-hover:text-gray-500 active:text-gray-600"
+                    />
+                )}
+            </Container>
+            {isExpanded && children}
+        </div>
+    );
+};
+
+export default Expander;
