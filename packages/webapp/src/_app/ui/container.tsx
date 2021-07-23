b4fe55a1e5da30b445dab89ceae7de51000cd32f
Brandon Fancher <brandonfancher@gmail.com>
2021-07-23 12:47:54 -0400

    Add optional darkBg prop to Container component.

--- packages/webapp/src/_app/ui/container.tsx
+++ packages/webapp/src/_app/ui/container.tsx
@@ -1,11 +1,20 @@
 interface Props {
     children: React.ReactNode;
+    darkBg?: boolean;
     onClick?: () => void;
     className?: string;
 }
 
-export const Container = ({ children, onClick, className = "" }: Props) => (
-    <div className={`p-2.5 ${className}`} onClick={onClick}>
+export const Container = ({
+    children,
+    darkBg,
+    onClick,
+    className = "",
+}: Props) => (
+    <div
+        className={`p-2.5 ${darkBg ? "bg-gray-100" : ""} ${className}`}
+        onClick={onClick}
+    >
         {children}
     </div>
 );
