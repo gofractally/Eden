0123bb14c351b056234809b66616a371fd691e8a
Brandon Fancher <brandonfancher@gmail.com>
2021-07-28 19:51:24 -0400

    Add ability to lock Expander component.

--- packages/webapp/src/_app/ui/expander.tsx
+++ packages/webapp/src/_app/ui/expander.tsx
@@ -8,6 +8,7 @@ interface ExpanderProps {
     showContentDivider?: boolean;
     startExpanded?: boolean;
     inactive?: boolean;
+    locked?: boolean;
     children: React.ReactNode;
 }
 
@@ -16,35 +17,58 @@ export const Expander = ({
     showContentDivider = false,
     startExpanded = false,
     inactive,
+    locked = false,
     children,
 }: ExpanderProps) => {
     const [isExpanded, setIsExpanded] = useState<boolean>(startExpanded);
     const containerClass =
-        "flex justify-between items-center group cursor-pointer hover:bg-gray-100 active:bg-gray-200 select-none";
+        "flex justify-between items-center group select-none";
+    const interactionClass = !locked
+        ? "cursor-pointer hover:bg-gray-100 active:bg-gray-200"
+        : "";
     const contentDividerClass =
         isExpanded && showContentDivider ? "border-b border-gray-100" : "";
+
+    const onExpand = () => {
+        if (locked) return;
+        setIsExpanded(!isExpanded);
+    };
+
     return (
         <div className={inactive ? "bg-gray-50" : ""}>
             <Container
-                className={`${containerClass} ${contentDividerClass}`}
-                onClick={() => setIsExpanded(!isExpanded)}
+                className={`${containerClass} ${interactionClass} ${contentDividerClass}`}
+                onClick={onExpand}
                 darkBg={inactive}
             >
                 {header}
-                {isExpanded ? (
-                    <IoChevronUpSharp
-                        size={19}
-                        className="text-gray-500 group-hover:text-gray-500 active:text-gray-600"
-                    />
-                ) : (
-                    <IoChevronDownSharp
-                        size={19}
-                        className="text-gray-500 group-hover:text-gray-500 active:text-gray-600"
-                    />
-                )}
+                <ExpansionIndicator isExpanded={isExpanded} locked={locked} />
             </Container>
             {isExpanded && children}
         </div>
+    );
+};
+
+const ExpansionIndicator = ({
+    isExpanded,
+    locked,
+}: {
+    isExpanded: boolean;
+    locked: boolean;
+}) => {
+    if (locked) return <></>;
+    if (isExpanded)
+        return (
+            <IoChevronUpSharp
+                size={19}
+                className="text-gray-500 group-hover:text-gray-500 active:text-gray-600"
+            />
+        );
+    return (
+        <IoChevronDownSharp
+            size={19}
+            className="text-gray-500 group-hover:text-gray-500 active:text-gray-600"
+        />
     );
 };
 
