83491c84954f13893a55323d9560f1bfa1093be3
Brandon Fancher <brandonfancher@gmail.com>
2021-08-02 12:53:49 -0400

    Remove election fee from genesis init community script.

--- scripts/init_community.sh
+++ scripts/init_community.sh
@@ -16,7 +16,6 @@ GENESIS_MEMO="A community is born."
 GENESIS_MEMBERS='"alice.edev", "pip.edev", "egeon.edev","edenmember11", "edenmember12", "edenmember13", "edenmember14", "edenmember15","edenmember21", "edenmember22", "edenmember23", "edenmember24", "edenmember25","edenmember31", "edenmember32", "edenmember33", "edenmember34", "edenmember35","edenmember41", "edenmember42"'
 ELECTION_DAY="0"
 ELECTION_TIME="16:00"
-ELECTION_DONATION="1.00000000 WAX"
 
 GENESIS_ACTION_DATA="{
     \"community\": \"$COMMUNITY_NAME\",
@@ -29,7 +28,6 @@ GENESIS_ACTION_DATA="{
     \"memo\": \"$GENESIS_MEMO\",
     \"election_day\": \"$ELECTION_DAY\",
     \"election_time\": \"$ELECTION_TIME\",
-    \"election_donation\": \"$ELECTION_DONATION\",
     \"collection_attributes\": [
         {
             \"key\": \"name\",
