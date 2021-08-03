4fb30b0729fbd8398cc00072bb3f6e469d2f6cf9
Brandon Fancher <brandonfancher@gmail.com>
2021-08-03 18:01:50 -0400

    Simply RoundHeader interface; push props down.

--- packages/webapp/src/elections/components/ongoing-election-components/round-header.tsx
+++ packages/webapp/src/elections/components/ongoing-election-components/round-header.tsx
@@ -1,24 +1,28 @@
 import { FaCheckCircle } from "react-icons/fa";
 import { GoSync } from "react-icons/go";
-import { Dayjs } from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 import { Text } from "_app/ui";
 import { CountdownPieMer } from "elections";
 
-// TODO: Make more data-driven. E.g., infer if round is active based on time passed in, etc. Props will change.
+type RoundHeaderProps =
+    | { roundData: any; roundNum?: number; subText?: string }
+    | { roundData?: any; roundNum: number; subText: string };
+
 export const RoundHeader = ({
+    roundData,
     roundNum,
-    isActive,
-    startDateTime,
-    endDateTime,
     subText,
-}: {
-    roundNum: number;
-    isActive?: boolean;
-    startDateTime?: Dayjs;
-    endDateTime?: Dayjs;
-    subText: string;
-}) => {
+}: RoundHeaderProps) => {
+    const isActive = Boolean(roundData);
+    const endsAt = isActive && dayjs(roundData.round_end + "Z");
+    const startsAt = endsAt && endsAt.subtract(40, "minute");
+    let subHeader = subText;
+
+    if (!subText && startsAt && endsAt) {
+        subHeader = `${startsAt.format("LT")} - ${endsAt.format("LT z")}`;
+    }
+
     return (
         <div className="w-full flex justify-between">
             <div className="flex items-center space-x-2">
@@ -29,17 +33,17 @@ export const RoundHeader = ({
                 )}
                 <div>
                     <Text size="sm" className="font-semibold">
-                        Round {roundNum}
+                        Round {roundNum ?? roundData.round}
                     </Text>
                     <Text size="sm" className="tracking-tight">
-                        {subText}
+                        {subHeader}
                     </Text>
                 </div>
             </div>
-            {isActive && startDateTime && endDateTime && (
+            {isActive && startsAt && endsAt && (
                 <CountdownPieMer
-                    startTime={startDateTime.toDate()}
-                    endTime={endDateTime.toDate()}
+                    startTime={startsAt.toDate()}
+                    endTime={endsAt.toDate()}
                 />
             )}
         </div>
