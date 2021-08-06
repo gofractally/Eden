fc0c634552820829778ff6f97e1919dd40ceae3e
Brandon Fancher <brandonfancher@gmail.com>
2021-08-06 18:11:57 -0400

    Style up the election screen when election is pending.

--- packages/webapp/src/elections/components/registration-election-components/index.ts
+++ packages/webapp/src/elections/components/registration-election-components/index.ts
@@ -1,1 +1,2 @@
+export * from "./election-faq";
 export * from "./participation-card";
--- packages/webapp/src/pages/election/index.tsx
+++ packages/webapp/src/pages/election/index.tsx
@@ -1,14 +1,29 @@
+import { FluidLayout, useCurrentElection } from "_app";
+import { Container, Heading } from "_app/ui";
 import { OngoingElection, RegistrationElection } from "elections";
-import { useCurrentElection } from "_app";
+import { EncryptionPasswordAlert } from "encryption";
 
 export const ElectionPage = () => {
     const { data: currentElection } = useCurrentElection();
+
     // TODO: Enum for election states?
-    return currentElection?.electionState ===
-        "current_election_state_registration" ? (
-        <RegistrationElection />
-    ) : (
-        <OngoingElection />
+    return (
+        <FluidLayout
+            title="Election"
+            banner={<EncryptionPasswordAlert promptSetupEncryptionKey />}
+        >
+            <div className="divide-y">
+                <Container>
+                    <Heading size={1}>Election</Heading>
+                </Container>
+                {currentElection?.electionState ===
+                "current_election_state_registration" ? (
+                    <RegistrationElection />
+                ) : (
+                    <OngoingElection />
+                )}
+            </div>
+        </FluidLayout>
     );
 };
 
