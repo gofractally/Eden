5b0d205b9b9aa28aff22e3cde3de965db3711863
Mike Manfredi <mike@clarionos.com>
2021-08-09 12:09:12 -0500

    removing now-unnecessary blank  scenario

--- packages/webapp/src/_app/api/eden-contract.ts
+++ packages/webapp/src/_app/api/eden-contract.ts
@@ -3,9 +3,7 @@ import { CONTRACT_GLOBAL_TABLE, getTableRows } from "_app";
 // check that member has participated in an election (if there's been one yet) (!="zzz...") and came to consensus with their group in the last election (!=0)
 const MEMBER_REPRESENTATIVE_IF_NOT_PARTICIPATED_IN_RECENT_ELECTION =
     "zzzzzzzzzzzzj";
-const MEMBER_REPRESENTATIVE_IF_FAILED_TO_REACH_CONSENSUS = "";
 export const isValidDelegate = (memberRep: string) =>
-    memberRep !== MEMBER_REPRESENTATIVE_IF_FAILED_TO_REACH_CONSENSUS &&
     memberRep !==
         MEMBER_REPRESENTATIVE_IF_NOT_PARTICIPATED_IN_RECENT_ELECTION &&
     // groups that didn't come to consensus will be assigned a randomly-generated value for representative
