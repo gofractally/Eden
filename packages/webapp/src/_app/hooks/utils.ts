19ee5c71b7bf03d1355904ba3e088256216afab3
Brandon Fancher <brandonfancher@gmail.com>
2021-08-12 18:58:03 -0400

    Create useTimeout hook.

--- packages/webapp/src/_app/hooks/utils.ts
+++ packages/webapp/src/_app/hooks/utils.ts
@@ -29,6 +29,27 @@ interface Countdown {
     onEnd?: () => void;
 }
 
+export const useTimeout = (callback: () => void, delay: number | null) => {
+    const savedCallback = useRef(callback);
+
+    // Remember the latest callback if it changes.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the timeout.
+    useEffect(() => {
+        // Don't schedule if no delay is specified.
+        if (delay === null) {
+            return;
+        }
+
+        const id = setTimeout(() => savedCallback.current(), delay);
+
+        return () => clearTimeout(id);
+    }, [delay]);
+};
+
 export const useCountdown = ({ startTime, endTime, onEnd }: Countdown) => {
     const [percentDecimal, setPercent] = useState<number>(0);
 
