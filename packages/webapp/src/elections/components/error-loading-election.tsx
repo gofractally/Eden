618ebbeebdb74c8c25f0b6f0cc5166fcce722be3
Brandon Fancher <brandonfancher@gmail.com>
2021-08-16 10:36:27 -0400

    PR feedback; add error handler on election page top level component.

--- packages/webapp/src/elections/components/error-loading-election.tsx
+++ packages/webapp/src/elections/components/error-loading-election.tsx
@@ -2,7 +2,7 @@ import { Container, Heading, Text } from "_app/ui";
 
 export const ErrorLoadingElection = () => (
     <Container className="flex flex-col justify-center items-center py-16">
-        <Heading size={4}>Error Loading Election</Heading>
+        <Heading size={4}>Error loading election information</Heading>
         <Text>Please reload the page to try again.</Text>
     </Container>
 );
