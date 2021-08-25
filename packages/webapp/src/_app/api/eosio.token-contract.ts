ccf75a7339052bfde4b5f143554eafaf6f12e199
Spark Plug <sparkplug0025@gmail.com>
2021-08-25 08:51:26 -0400

    Add token balance and history button in profile

---
+++ packages/webapp/src/_app/api/eosio.token-contract.ts
@@ -0,0 +1,21 @@
+import { minimumDonationAmount } from "config";
+import { Asset, assetFromString } from "_app/utils";
+import { eosJsonRpc } from "../eos";
+
+export const getTokenBalanceForAccount = async (
+    account: string
+): Promise<Asset> => {
+    const data = await eosJsonRpc.get_currency_balance(
+        "eosio.token",
+        account,
+        minimumDonationAmount.symbol
+    );
+
+    console.info("eosio.token", data);
+
+    if (data && data.length) {
+        return assetFromString(data[0]);
+    } else {
+        return { ...minimumDonationAmount, quantity: 0.0 };
+    }
+};
---
+++ packages/webapp/src/members/components/token-balance.tsx
@@ -0,0 +1,29 @@
+import { assetToString, Link, Text, useTokenBalanceForAccount } from "_app";
+import { blockExplorerAccountBaseUrl } from "config";
+
+import { MemberData } from "../interfaces";
+
+interface Props {
+    member: MemberData;
+}
+
+export const TokenBalance = ({ member }: Props) => {
+    const { data: balance } = useTokenBalanceForAccount(member.account);
+
+    return (
+        <div>
+            <Text>
+                <strong>Balance:</strong>{" "}
+                {balance ? assetToString(balance) : "loading..."}{" "}
+                <span className="ml-4">
+                    <Link
+                        href={`${blockExplorerAccountBaseUrl}/${member.account}`}
+                        target="_blank"
+                    >
+                        History
+                    </Link>
+                </span>
+            </Text>
+        </div>
+    );
+};
