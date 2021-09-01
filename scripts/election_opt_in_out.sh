250affd449df613553a043eecd9f5b036b8f60e2
Brandon Fancher <brandonfancher@gmail.com>
2021-09-01 14:31:05 -0400

    Update script endpoints and password to new format.

--- scripts/election_opt_in_out.sh
+++ scripts/election_opt_in_out.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env sh
 set -xe
 
-CLEOS="cleos -u https://wax-test.eosdac.io"
+CLEOS="cleos -u https://waxtest.eosn.io"
 
 CONTRACT="test2.edev"
 PARTICIPATING=true
 SET_PUB_KEY=true
-PUB_KEY="PUB_K1_6aB7nuqWvrD4oKEi6pcr3PRis9UiMoRKmkQRKT7MF9jyxhEaxQ"
+PUB_KEY="EOS676NTBDHP5dTZW8KftWfj5YtU5HUcFd4MsGrSJdTiVpn1RV7Lm"
 
-# PUBLIC KEY:
-  # PUB_K1_6aB7nuqWvrD4oKEi6pcr3PRis9UiMoRKmkQRKT7MF9jyxhEaxQ
-  # EOS6aB7nuqWvrD4oKEi6pcr3PRis9UiMoRKmkQRKT7MF9jyutesMh
-# PRIVATE KEY:
-  # PVT_K1_pVwv4xcJdkKweWE6Ba1hPwUUDcu6EsM56t7pXJkh89kk5Wn1s
-  # 5JdnXbX3gmCjax2nGHdU6k94h53Y7E6x4tAfLza3zZynvHQzCWw
+# PUBLIC KEY: EOS676NTBDHP5dTZW8KftWfj5YtU5HUcFd4MsGrSJdTiVpn1RV7Lm
+# PRIVATE KEY: 5JuyKTpt9xrD5shspHJRC5qNwku8yEREt1UeUBB5hLxbU7S3Fvq
+# PASSWORD: %yCLM7RDwidC
 
 for PARMS in $(cat ./genesis_accounts_long_list); do
   ACCOUNT_NAME=$(echo $PARMS | cut -d "," -f1);
