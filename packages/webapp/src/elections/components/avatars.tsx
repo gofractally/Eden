0aa17da2298a322e41fc5f8c75b155707636e3c4
Brandon Fancher <brandonfancher@gmail.com>
2021-09-02 18:07:10 -0400

    Add avatars to election participation card.

---
+++ packages/webapp/src/elections/components/avatars.tsx
@@ -0,0 +1,38 @@
+export const Avatars = () => {
+    return (
+        <div className="flex justify-between py-2">
+            <img
+                src="/images/avatars/avatar-1.svg"
+                className="w-1/6 xs:w-1/12"
+            />
+            <img
+                src="/images/avatars/avatar-2.svg"
+                className="w-1/6 xs:w-1/12"
+            />
+            <img
+                src="/images/avatars/avatar-3.svg"
+                className="w-1/6 xs:w-1/12"
+            />
+            <img
+                src="/images/avatars/avatar-4.svg"
+                className="w-1/6 xs:w-1/12"
+            />
+            <img
+                src="/images/avatars/avatar-5.svg"
+                className="w-1/6 xs:w-1/12"
+            />
+            <img
+                src="/images/avatars/avatar-6.svg"
+                className="w-1/12 hidden xs:block"
+            />
+            <img
+                src="/images/avatars/avatar-7.svg"
+                className="w-1/12 hidden xs:block"
+            />
+            <img
+                src="/images/avatars/avatar-8.svg"
+                className="w-1/12 hidden sm:block"
+            />
+        </div>
+    );
+};
