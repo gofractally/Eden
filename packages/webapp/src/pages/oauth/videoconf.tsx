52cdb78f2cc009e2c204f4a01c1e5f3f32872815
Brandon Fancher <brandonfancher@gmail.com>
2021-09-03 18:19:09 -0400

    A stab at Privacy policy and Terms of use for Zoom integration.

--- packages/webapp/src/pages/oauth/videoconf.tsx
+++ packages/webapp/src/pages/oauth/videoconf.tsx
@@ -99,10 +99,50 @@ export const ZoomOauthPage = ({ newZoomAccountJWT, oauthState }: Props) => {
                 </CallToAction>
             )}
             <Expander header={<Heading size={3}>Privacy policy</Heading>}>
-                <Container>This is a test</Container>
+                <Container className="space-y-2.5">
+                    <Text>
+                        Your Zoom account will be used to schedule Eden election
+                        meetings on your behalf for members participating in
+                        your Eden election groups during the course of an
+                        ongoing election.
+                    </Text>
+                    <Text>
+                        NONE of your personal information nor any credentials,
+                        tokens or keys relating to your Zoom account are ever
+                        stored, persisted or retained on any server or database.
+                        The only information used by EdenOS is your Zoom OAuth
+                        token, and that is stored only in your local web
+                        browser.
+                    </Text>
+                    <Text>
+                        Whenever you request that an election meeting be
+                        scheduled by EdenOS on your behalf, that OAuth token is
+                        sent, ephemerally, to a stateless, serverless function
+                        which creates the meeting on your Zoom account on your
+                        behalf. Your token is then immediately forgotten by the
+                        server.
+                    </Text>
+                    <Text>
+                        This means that resetting your browser or clearing your
+                        browser's cache or settings is sufficient to deny the
+                        EdenOS application further access to your OAuth token
+                        and prevent it from creating further meetings on your
+                        Zoom account on your behalf.
+                    </Text>
+                </Container>
             </Expander>
             <Expander header={<Heading size={3}>Terms of use</Heading>}>
-                <Container>This is a test</Container>
+                <Container className="space-y-2.5">
+                    <Text>
+                        By using the EdenOS Genesis app integration with Zoom,
+                        you understand that the EdenOS software will schedule
+                        meetings on your behalf (but specifically and only when
+                        you request that a meeting be created). The resulting
+                        meeting link/URL will be encrypted and distributed to
+                        your election meeting co-participants. And you accept
+                        that.
+                    </Text>
+                </Container>
             </Expander>
             <Expander header={<Heading size={3}>Documentation</Heading>}>
                 <Container>This is a test</Container>
