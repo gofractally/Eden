1d1b51238dff0d129d42552be8bc4c102eb65bb3
Brandon Fancher <brandonfancher@gmail.com>
2021-09-23 16:30:22 -0400

    Tweak consensometer colors and layout.

--- packages/webapp/src/elections/components/ongoing-election-components/consensometer.tsx
+++ packages/webapp/src/elections/components/ongoing-election-components/consensometer.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import { GiCheckeredFlag } from "react-icons/gi";
 import { FaStar } from "react-icons/fa";
 
-import { Text } from "_app/ui";
+import { Heading, Text } from "_app/ui";
 import { VoteData } from "elections/interfaces";
 import { tallyVotesFromVoteData } from "elections/utils";
 
@@ -71,55 +72,63 @@ export const Consensometer = ({ voteData }: Props) => {
     }
 
     return (
-        <div className="flex flex-col items-end space-y-1 pt-1">
-            <div className="flex space-x-1">
-                {Array.from({ length: totalVotesCast }).map((_, i) => {
-                    let thisBlock = i + 1;
-                    let color = "bg-gray-300";
+        <div className="flex flex-col space-y-1 pt-1">
+            <div className="flex items-center space-x-3">
+                <Heading size={4} className="inline-block">
+                    Consensus
+                </Heading>
+                <div className="flex space-x-1">
+                    {Array.from({ length: totalVotesCast }).map((_, i) => {
+                        let thisBlock = i + 1;
+                        let color = "bg-gray-300";
 
-                    // Blocks represent votes cast
-                    // Number of blue blocks represents votes cast for leading candidate(s)
-                    // Green blocks are votes cast for the leading candidate once consensus is reached
-                    // Finish line/flag block represents number of votes required to reach consensus
-                    // Finish line/flag block is skipped if number of votes to reach consensus is attained but lead candidate is not voting for themself
+                        // Blocks represent votes cast
+                        // Number of blue blocks represents votes cast for leading candidate(s)
+                        // Green blocks are votes cast for the leading candidate once consensus is reached
+                        // Finish line/flag block represents number of votes required to reach consensus
+                        // Finish line/flag block is skipped if number of votes to reach consensus is attained but lead candidate is not voting for themself
 
-                    if (leaderIsVotingForSelf) {
-                        if (thisBlock <= leadTally && isThereConsensus) {
-                            color = "bg-green-500";
-                        } else if (thisBlock <= leadTally) {
-                            color = "bg-blue-500";
+                        if (leaderIsVotingForSelf) {
+                            if (thisBlock <= leadTally && isThereConsensus) {
+                                color = "bg-green-400";
+                            } else if (thisBlock <= leadTally) {
+                                color = "bg-blue-400";
+                            }
+                        } else if (
+                            thisBlock !== totalVotesRequiredForConsensus
+                        ) {
+                            // apply blue blocks like before but skip/ignore the finish line block
+                            if (thisBlock > totalVotesRequiredForConsensus) {
+                                thisBlock = i;
+                            }
+                            if (thisBlock <= leadTally) {
+                                color = "bg-blue-400";
+                            }
                         }
-                    } else if (thisBlock !== totalVotesRequiredForConsensus) {
-                        // apply blue blocks like before but skip/ignore the finish line block
-                        if (thisBlock > totalVotesRequiredForConsensus) {
-                            thisBlock = i;
-                        }
-                        if (thisBlock <= leadTally) {
-                            color = "bg-blue-500";
-                        }
-                    }
 
-                    return (
-                        <div
-                            key={"consensometer - " + i}
-                            className={`flex justify-center items-center w-9 h-5 rounded ${color}`}
-                        >
-                            {i + 1 === totalVotesRequiredForConsensus &&
-                                (isThereConsensus ? (
-                                    <FaStar
-                                        size={14}
-                                        className="text-yellow-500"
-                                    />
-                                ) : (
-                                    <GiCheckeredFlag size={16} color="black" />
-                                ))}
-                        </div>
-                    );
-                })}
+                        return (
+                            <div
+                                key={"consensometer - " + i}
+                                className={`flex justify-center items-center w-9 h-5 rounded ${color}`}
+                            >
+                                {i + 1 === totalVotesRequiredForConsensus &&
+                                    (isThereConsensus ? (
+                                        <FaStar
+                                            size={14}
+                                            className="text-white"
+                                        />
+                                    ) : (
+                                        <GiCheckeredFlag
+                                            size={16}
+                                            color="black"
+                                        />
+                                    ))}
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
-            <Text size="sm" type="note">
-                {helpText}
-            </Text>
+            <Text>{helpText}</Text>
         </div>
     );
 };
