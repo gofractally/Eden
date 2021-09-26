6db705e3a2d3bfde58706f4b8eef7d4a33a3f5cf
SparkPlug0025 <79721020+sparkplug0025@users.noreply.github.com>
2021-09-26 10:41:41 -0400

    Add Free Meeting Link functionality (#496)
    * Add Free Meeting Link functionality
    
    * fix free meeting link input style
    
    * Restrict meeting links to zoom
    
    * fix regex
    
    * Address PR feedback
    
    * address PR feedback

--- packages/webapp/src/elections/components/ongoing-election-components/meeting-link/meeting-link.tsx
+++ packages/webapp/src/elections/components/ongoing-election-components/meeting-link/meeting-link.tsx
@@ -19,6 +19,8 @@ import { useEncryptionPassword, usePasswordModal } from "encryption";
 
 import MeetingButtons from "./meeting-buttons";
 import MeetingLinkModal from "./meeting-link-modal";
+import { freeformMeetingLinksEnabled } from "config";
+import { validateMeetingLink } from "_app/utils/meetings";
 
 export enum MeetingStep {
     LinkZoomAccount,
@@ -76,7 +78,7 @@ export const MeetingLink = ({
     let meetingStep = MeetingStep.LinkZoomAccount;
     if (encryptedData) {
         meetingStep = MeetingStep.RetrieveMeetingLink;
-    } else if (zoomAccountJWT) {
+    } else if (zoomAccountJWT || freeformMeetingLinksEnabled) {
         meetingStep = MeetingStep.CreateMeetingLink;
     }
 
@@ -89,9 +91,9 @@ export const MeetingLink = ({
     };
 
     const handleMeetingButton = async () => {
-        const linkAlreadyExists =
-            meetingStep === MeetingStep.RetrieveMeetingLink;
         if (isPasswordNotSet || isPasswordSetNotPresent) {
+            const linkAlreadyExists =
+                meetingStep === MeetingStep.RetrieveMeetingLink;
             const completed = await showPasswordModal(linkAlreadyExists);
             // if the user canceled, do nothing -- just dismiss
             completed && !linkAlreadyExists && showMeetingLinkModal();
@@ -100,46 +102,60 @@ export const MeetingLink = ({
         }
     };
 
-    const requestMeetingLink = async (throwOnError: boolean = false) => {
+    // TODO: Should we just pass the participants/recipients into this component?
+    const recipientAccounts = memberGroup
+        .map((member) => member.member)
+        .filter((account) => account !== ualAccount.accountName);
+
+    const checkSubmissionIsAllowed = async () => {
+        // check all the participants keys are ready to be encrypted
+        // if this dummy encryption fails we know we can't create the meeting
+        await encryptSecretForPublishing(
+            "dummy-encryption-test",
+            ualAccount.accountName,
+            recipientAccounts
+        );
+
+        // eagerly check for presence of link on chain to avoid creating
+        // unnecessary meetings and making user sign something for nothing.
+        const { data } = await refetchEncryptedData();
+        // TODO: attempt to decrypt here; if unsuccessful, throw error and inform user they're new password will only work for upcoming rounds / elections
+
+        if (data) return;
+    };
+
+    const submitMeetingLink = async (meetingLink: string) => {
+        validateMeetingLink(meetingLink);
+        await checkSubmissionIsAllowed();
+        await signAndPublishMeeting(meetingLink);
+    };
+
+    const requestMeetingLink = async () => {
+        await checkSubmissionIsAllowed();
+
+        const topic = `Eden Election - Round #${roundIndex + 1}`;
+        const durationInMinutes = meetingDurationMs / 1000 / 60;
+
+        const responseData = await generateZoomMeetingLink(
+            zoomAccountJWT,
+            setZoomAccountJWT,
+            topic,
+            durationInMinutes,
+            meetingStartTime.toISOString()
+        );
+
+        console.info("generated meeting data", responseData);
+        if (!responseData.meeting || !responseData.meeting.join_url) {
+            throw new Error("Invalid generated Meeting Link URL");
+        }
+
+        await signAndPublishMeeting(responseData.meeting.join_url);
+    };
+
+    const signAndPublishMeeting = async (meetingLink: string) => {
         try {
-            // TODO: Should we just pass the participants/recipients into this component?
-            const recipientAccounts = memberGroup
-                .map((member) => member.member)
-                .filter((account) => account !== ualAccount.accountName);
-
-            // check all the participants keys are ready to be encrypted
-            // if this dummy encryption fails we know we can't create the meeting
-            await encryptSecretForPublishing(
-                "dummy-encryption-test",
-                ualAccount.accountName,
-                recipientAccounts
-            );
-
-            // eagerly check for presence of link on chain to avoid creating
-            // unnecessary meetings and making user sign something for nothing.
-            const { data } = await refetchEncryptedData();
-            // TODO: attempt to decrypt here; if unsuccessful, throw error and inform user they're new password will only work for upcoming rounds / elections
-
-            if (data) return;
-
-            const topic = `Eden Election - Round #${roundIndex + 1}`;
-            const durationInMinutes = meetingDurationMs / 1000 / 60;
-
-            const responseData = await generateZoomMeetingLink(
-                zoomAccountJWT,
-                setZoomAccountJWT,
-                topic,
-                durationInMinutes,
-                meetingStartTime.toISOString()
-            );
-
-            console.info("generated meeting data", responseData);
-            if (!responseData.meeting || !responseData.meeting.join_url) {
-                throw new Error("Invalid generated Meeting Link URL");
-            }
-
             const encryptedMeetingData = await encryptSecretForPublishing(
-                responseData.meeting.join_url,
+                meetingLink,
                 ualAccount.accountName,
                 recipientAccounts
             );
@@ -160,17 +176,16 @@ export const MeetingLink = ({
             console.info("electmeeting trx", signedTrx);
         } catch (e) {
             const error = e as Error;
-            console.error(error);
             const message = error.toString();
             const linkAlreadyExistsError = message?.includes(
                 "Encrypted data does not match"
             );
             // if encrypted data is already on chain; fail silently and fetch new data
             if (!linkAlreadyExistsError) {
-                onError(error);
-                if (throwOnError) throw error;
+                throw error;
             }
         }
+
         // refetch updated encrypted data after allowing time for chains/nodes to see it
         await delay(3000);
         refetchEncryptedData();
@@ -190,6 +205,7 @@ export const MeetingLink = ({
                 close={() => setShowMeetingModal(false)}
                 meetingStep={meetingStep}
                 requestMeetingLink={requestMeetingLink}
+                submitMeetingLink={submitMeetingLink}
                 stage={stage}
             />
         </>
