75d81c1e9a3ba84d16f6fa7702afd128c7c4bf1c
Brandon Fancher <brandonfancher@gmail.com>
2021-09-29 16:59:58 -0400

    Update scripts with examples and better friendly names.

--- scripts/genesis_profiles.sh
+++ scripts/genesis_profiles.sh
@@ -8,7 +8,8 @@ for PARMS in $(cat ./genesis_accounts_long_list); do
   ACCOUNT_NAME=$(echo $PARMS | cut -d "," -f1);
   INDUCTION_ID=$(echo $PARMS | cut -d "," -f2);
   IMAGE=$(echo $PARMS | cut -d "," -f3);
-  NAME="Test Member $INDUCTION_ID"
+  FNAME=$(echo $PARMS | cut -d "," -f4);
+  LNAME=$(echo $PARMS | cut -d "," -f5);
   BIO="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis, nulla laoreet vulputate pharetra, eros ex finibus urna, ut semper neque eros eget turpis. Proin semper fringilla lobortis. Phasellus quis sapien nec dolor porta feugiat in eget augue. Etiam porta dolor sed leo tristique mattis sollicitudin sed neque. Proin in iaculis nunc. Integer placerat suscipit malesuada. Nunc pretium in urna ac gravida. Nulla in velit dignissim, bibendum est ut, auctor enim. Vestibulum non aliquet turpis. Etiam mattis, urna id scelerisque aliquet, nibh turpis pulvinar odio, sed tincidunt nisi libero et sem. Integer ultrices, nulla a sollicitudin efficitur, turpis ipsum semper ipsum, et efficitur leo est ut leo. Suspendisse massa ex, venenatis pretium nulla ut, sodales sagittis lectus."
 
   INVITEE=$ACCOUNT_NAME
@@ -23,7 +24,7 @@ for PARMS in $(cat ./genesis_accounts_long_list); do
   PROFILE_ACTION_DATA="{
     \"id\": $INDUCTION_ID,
     \"new_member_profile\": {
-      \"name\": \"$NAME\",
+      \"name\": \"$FNAME $LNAME\",
       \"img\": \"$IMAGE\",
       \"bio\": \"$BIO\",
       \"social\": \"{\\\"eosCommunity\\\":\\\"$SOCIAL_EOSCOMMUNITY\\\",\\\"twitter\\\":\\\"$SOCIAL_TWITTER\\\",\\\"linkedin\\\":\\\"$SOCIAL_LINKEDIN\\\",\\\"telegram\\\":\\\"$SOCIAL_TELEGRAM\\\",\\\"facebook\\\":\\\"$SOCIAL_FACEBOOK\\\",\\\"blog\\\":\\\"$SOCIAL_BLOG\\\"}\",
