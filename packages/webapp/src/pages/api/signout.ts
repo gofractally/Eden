d149454e4e06a62c7c512094e1d584abebed93d7
SparkPlug0025 <sparkplug0025@gmail.com>
2021-10-01 09:21:15 -0400

    signout json response

--- packages/webapp/src/pages/api/signout.ts
+++ packages/webapp/src/pages/api/signout.ts
@@ -18,7 +18,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 const handleSignOut = async (_req: NextApiRequest, res: NextApiResponse) => {
     try {
         setZoomJWTCookie(null, res); // clear zoom JWT cookie
-        res.send("success");
+        res.status(200).json({ success: "ok" });
     } catch (error) {
         console.error(error);
         return handleErrors(res, error as Error);
