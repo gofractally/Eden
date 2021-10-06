8f728d9bf7a54b14f20fbb1030972201ec8ed42e
Brandon Fancher <brandonfancher@gmail.com>
2021-10-06 18:52:16 -0400

    Tweak tailwind to expose their mono and serif fonts.

--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
             },
         },
         fontFamily: {
+            ...defaultTheme.fontFamily,
             sans: [
                 "Inter",
                 "ui-sans-serif",
