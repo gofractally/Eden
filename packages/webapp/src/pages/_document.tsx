7de1a849747bd44d4e41c99ac87dc82cb1c58ab6
Brandon Fancher <brandonfancher@gmail.com>
2021-10-06 16:16:38 -0400

    Add lang tag to html document.

--- packages/webapp/src/pages/_document.tsx
+++ packages/webapp/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="en">
                 <Head>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900"
