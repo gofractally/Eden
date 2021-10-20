7eb8ed869d0f0a6e029158f0f5b33c7e8556bbd4
Brandon Fancher <brandonfancher@gmail.com>
2021-10-20 12:19:09 -0400

    Process PR feedback; remove comment.

--- packages/webapp/src/pages/_app.tsx
+++ packages/webapp/src/pages/_app.tsx
@@ -24,7 +24,7 @@ import { UalSoftkeyLoginModal } from "_app/eos/ual/softkey";
 import "tailwindcss/tailwind.css";
 import "_app/styles/nprogress.tailwind.css";
 import "_app/styles/add-to-calendar.chq.css";
-import "react-virtualized/styles.css"; // only needs to be imported once
+import "react-virtualized/styles.css";
 
 Router.events.on("routeChangeStart", (url) => {
     console.log(`Loading: ${url}`);
