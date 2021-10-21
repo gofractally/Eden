14949a7c6aed6c1aead0c3801596572c632b55aa
Brandon Fancher <brandonfancher@gmail.com>
2021-10-20 23:24:15 -0400

    Adjust members search layout for desktop.

--- packages/webapp/src/_app/hooks/utils.ts
+++ packages/webapp/src/_app/hooks/utils.ts
@@ -93,3 +93,36 @@ export const usePrevious = <T>(value: T): T => {
     }, [value]);
     return ref.current;
 };
+
+// Define general type for useWindowSize hook, which includes width and height
+interface Size {
+    width: number | undefined;
+    height: number | undefined;
+}
+
+// https://usehooks.com/useWindowSize/
+export const useWindowSize = (): Size => {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState<Size>({
+        width: undefined,
+        height: undefined,
+    });
+    useEffect(() => {
+        // Handler to call on window resize
+        const handleResize = () => {
+            // Set window width/height to state
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+};
--- packages/webapp/src/pages/members/index.tsx
+++ packages/webapp/src/pages/members/index.tsx
@@ -16,6 +16,8 @@ import {
     LoadingContainer,
     Text,
     useFormFields,
+    SetValuesEvent,
+    useWindowSize,
 } from "_app";
 import { MemberChip, MemberData } from "members";
 import { CircleX, MagnifyingGlass } from "_app/ui/icons";
@@ -28,14 +30,71 @@ interface Props {
 
 export const MembersPage = (props: Props) => (
     <SideNavLayout title="Community" className="relative">
-        <Container className="lg:sticky lg:top-0 lg:z-10 bg-white">
-            <Heading size={1}>Community</Heading>
-        </Container>
         <AllMembers />
     </SideNavLayout>
 );
 
+interface CommunityHeaderProps {
+    className?: string;
+    children?: React.ReactNode;
+}
+
+const CommunityHeader = ({
+    className = "",
+    children,
+}: CommunityHeaderProps) => (
+    <Container
+        className={`flex items-center justify-between ${className}`}
+        style={{ height: 76, paddingTop: 0, paddingBottom: 0 }}
+    >
+        <Heading size={1}>Community</Heading>
+        {children}
+    </Container>
+);
+
+interface SearchProps {
+    fields: {
+        memberSearch: string;
+    };
+    setFields: SetValuesEvent;
+    className?: string;
+}
+
+const MembersSearch = ({ fields, setFields, className = "" }: SearchProps) => {
+    const clearSearch = () => {
+        setFields({ target: { id: "memberSearch", value: "" } });
+    };
+
+    return (
+        <div
+            className={`flex items-center pl-2.5 text-gray-300 focus-within:text-gray-400 transition ${className}`}
+        >
+            <MagnifyingGlass size={18} />
+            <input
+                id="memberSearch"
+                name="memberSearch"
+                type="text"
+                value={fields.memberSearch}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setFields(e)
+                }
+                className="flex-1 h-14 focus:ring-0 border-none placeholder-gray-300 text-lg"
+                placeholder="find member"
+            />
+            {fields.memberSearch ? (
+                <div
+                    className="flex items-center p-2.5 text-gray-400"
+                    onClick={clearSearch}
+                >
+                    <CircleX size={18} />
+                </div>
+            ) : null}
+        </div>
+    );
+};
+
 const AllMembers = () => {
+    const { width: windowWidth } = useWindowSize();
     const newMembers = useQuery({
         ...queryNewMembers(1, NEW_MEMBERS_PAGE_SIZE),
     });
@@ -47,32 +106,40 @@ const AllMembers = () => {
     });
 
     if (newMembers.isLoading || allMembers.isLoading) {
-        return <LoadingContainer />;
+        return (
+            <>
+                <CommunityHeader />
+                <LoadingContainer />
+            </>
+        );
     }
 
     if (newMembers.isError || allMembers.isError || !allMembers.data) {
         return (
-            <Container className="flex flex-col justify-center items-center py-16 text-center">
-                <Heading size={4}>Error loading member information</Heading>
-                <Text>Please reload the page to try again.</Text>
-            </Container>
+            <>
+                <CommunityHeader />
+                <Container className="flex flex-col justify-center items-center py-16 text-center">
+                    <Heading size={4}>Error loading member information</Heading>
+                    <Text>Please reload the page to try again.</Text>
+                </Container>
+            </>
         );
     }
 
     if (!(allMembers.data as MemberData[]).length) {
         return (
-            <Container className="flex flex-col justify-center items-center py-16 text-center">
-                <Heading size={4}>No members found</Heading>
-                <Text>
-                    There don't seem to be any members in this community yet.
-                </Text>
-            </Container>
+            <>
+                <CommunityHeader />
+                <Container className="flex flex-col justify-center items-center py-16 text-center">
+                    <Heading size={4}>No members found</Heading>
+                    <Text>
+                        There don't seem to be any members in this community
+                        yet.
+                    </Text>
+                </Container>
+            </>
         );
     }
-
-    const clearSearch = () => {
-        setFields({ target: { id: "memberSearch", value: "" } });
-    };
 
     const findMember = (member: MemberData) =>
         member.account.includes(fields.memberSearch.toLowerCase()) ||
@@ -93,34 +160,29 @@ const AllMembers = () => {
         .sort(sortMembers)
         .map(mergeAuctionData);
 
+    const XS_BREAKPOINT = 475; // TODO: Move to breakpoints file
+    const MOBILE_TOP_NAV_HEIGHT = 56;
+
     return (
         <>
+            <CommunityHeader className="lg:sticky lg:top-0 lg:z-10 bg-white border-b">
+                <MembersSearch
+                    fields={fields}
+                    setFields={setFields}
+                    className="hidden lg:flex flex-1 max-w-md border-b"
+                />
+            </CommunityHeader>
             <div
-                className="xs:hidden sticky z-10 bg-white"
-                style={{ top: 53, boxShadow: "0 0 0 1px #e5e5e5" }}
+                className="lg:hidden sticky z-10 bg-white"
+                style={{
+                    top:
+                        (windowWidth ?? 0) < XS_BREAKPOINT
+                            ? MOBILE_TOP_NAV_HEIGHT - 1
+                            : 0,
+                    boxShadow: "0 0 0 1px #e5e5e5",
+                }}
             >
-                <div className="flex items-center pl-2.5 text-gray-300 focus-within:text-gray-400 transition">
-                    <MagnifyingGlass size={18} />
-                    <input
-                        id="memberSearch"
-                        name="memberSearch"
-                        type="text"
-                        value={fields.memberSearch}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                            setFields(e)
-                        }
-                        className="flex-1 h-14 focus:ring-0 border-none placeholder-gray-300 text-lg"
-                        placeholder="find member"
-                    />
-                    {fields.memberSearch ? (
-                        <div
-                            className="flex items-center p-2.5 text-gray-400"
-                            onClick={clearSearch}
-                        >
-                            <CircleX size={18} />
-                        </div>
-                    ) : null}
-                </div>
+                <MembersSearch fields={fields} setFields={setFields} />
             </div>
             <WindowScroller>
                 {({
