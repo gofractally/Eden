8896468b3c642b49eba77e4bfe71bd69c96999ae
SparkPlug0025 <sparkplug0025@gmail.com>
2021-10-24 22:50:46 -0400

    Setup genesis ephemeral data

--- contracts/eden/tests/setup-genesis.cpp
+++ contracts/eden/tests/setup-genesis.cpp
@@ -3,8 +3,20 @@
 
 #include <tester_base.hpp>
 
+const std::string setup_state_path = "chain-genesis";
+
 TEST_CASE("Setup Eden chain with completed genesis")
 {
    eden_tester t;
    t.genesis();
+
+   // tolerance blocks before grabbing the log
+   t.chain.start_block();
+   t.chain.start_block();
+
+   // copy state
+   auto chain_path = t.chain.get_path();
+   eosio::execute("rm -rf " + setup_state_path);
+   eosio::execute("mkdir -p " + setup_state_path + "/blocks");
+   eosio::execute("cp " + chain_path + "/blocks/blocks.log " + setup_state_path + "/blocks");
 }
