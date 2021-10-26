71ba5d50716c12ffc2cc7f3e577f1d77713e434f
Brandon Fancher <brandonfancher@gmail.com>
2021-10-26 11:55:36 -0400

    Remove SSR from member detail page.

--- packages/webapp/src/pages/members/[id].tsx
+++ packages/webapp/src/pages/members/[id].tsx
@@ -1,6 +1,5 @@
-import { GetServerSideProps } from "next";
-import { QueryClient, useQuery } from "react-query";
-import { dehydrate } from "react-query/hydration";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
 
 import {
     CallToAction,
@@ -14,33 +13,12 @@ import { ROUTES } from "_app/routes";
 import { MemberCard, MemberCollections, MemberHoloCard } from "members";
 import { DelegateFundsAvailable } from "delegates/components";
 
-/**
- * We have an issue if the member is not found in the development environment
- * due to dehydration with JSON not being able to serialize `undefined`:
- * Error: Error serializing `.dehydratedState.queries[0].state.data`
- * returned from `getServerSideProps` in "/members/[id]".
- * Reason: `undefined` cannot be serialized as JSON. Please use `null`
- * or omit this value.
- * Let's track this here: https://github.com/tannerlinsley/react-query/issues/1978
- */
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-    const account = params!.id as string;
-
-    const queryClient = new QueryClient();
-    await queryClient.prefetchQuery(queryMemberData(account));
-
-    return { props: { account, dehydratedState: dehydrate(queryClient) } };
-};
-
-interface Props {
-    account: string;
-}
-
-export const MemberPage = ({ account }: Props) => {
-    const { data: member, isLoading } = useQuery({
-        ...queryMemberData(account),
-        keepPreviousData: true,
-    });
+export const MemberPage = () => {
+    const router = useRouter();
+    const { account } = router.query;
+    const { data: member, isLoading } = useQuery(
+        queryMemberData(account as string)
+    );
 
     if (member) {
         return (
@@ -48,7 +26,7 @@ export const MemberPage = ({ account }: Props) => {
                 title={`${member.name}'s Profile`}
                 className="divide-y"
             >
-                <DelegateFundsAvailable account={account} />
+                <DelegateFundsAvailable account={account as string} />
                 <Container className="space-y-2.5">
                     <div className="flex items-center space-y-10 xl:space-y-0 xl:space-x-4 flex-col">
                         <div className="max-w-xl">
