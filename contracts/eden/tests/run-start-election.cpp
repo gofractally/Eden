93f0269dc8594c7567818d9ed4ae631b047f2b7e
Brandon Fancher <brandonfancher@gmail.com>
2021-11-16 10:43:15 -0500

    Add ephemeral chain test to set up and start election.

---
+++ contracts/eden/tests/run-start-election.cpp
@@ -0,0 +1,15 @@
+#include <nodeos-runner.hpp>
+
+TEST_CASE("Setup Eden chain with start of full election")
+{
+   nodeos_runner r("chain-start-election");
+
+   r.tester.genesis();
+   r.tester.run_election(true, 10000, true);
+   r.checkpoint("small_election");
+   r.tester.induct_n(100);
+   r.checkpoint("inductions");
+   r.tester.start_election(true, 10000);
+
+   r.start_nodeos();
+}
