5c4b7e8481428ae7ac57f80ac87e4223ba966ec7
Brandon Fancher <brandonfancher@gmail.com>
2021-11-16 12:46:13 -0500

    Attempt to set election time.

--- contracts/eden/tests/run-start-election.cpp
+++ contracts/eden/tests/run-start-election.cpp
@@ -9,6 +9,7 @@ TEST_CASE("Setup Eden chain with start of full election")
    r.checkpoint("small_election");
    r.tester.induct_n(100);
    r.checkpoint("inductions");
+   r.tester.eden_gm.act<actions::electsettime>(s2t("2021-11-16T18:45:00.000"));
    r.tester.start_election(true, 10000);
 
    r.start_nodeos();
