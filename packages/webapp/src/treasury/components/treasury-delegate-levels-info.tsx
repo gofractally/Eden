c7b3c89fd2a1eba92263631a4737c6138a5593d7
shakhruz <shakhruz@gmail.com>
2022-03-18 00:27:17 +0500

    #611 changed wording 'Head-chief' to 'Head Chief' and 'Level 1 Delegate' instead of '1 Delegate' on Treasury page

--- packages/webapp/src/treasury/components/treasury-delegate-levels-info.tsx
+++ packages/webapp/src/treasury/components/treasury-delegate-levels-info.tsx
@@ -11,7 +11,7 @@ import {
 enum RankLabel {
     NumberedLevel = "N",
     Chief = "Chief",
-    HeadChief = "Head-chief",
+    HeadChief = "Head Chief",
 }
 
 export const TreasuryDelegateLevelsInfo = () => {
@@ -91,7 +91,7 @@ const RankLevelDistribution = ({
     amount,
     level,
 }: RankLevelDistributionProps) => {
-    const labelText = label === RankLabel.NumberedLevel ? `${level}` : label;
+    const labelText = label === RankLabel.NumberedLevel ? `Level ${level}` : label;
 
     return (
         <div className="flex justify-between">
