4656186a3f2fbe714554b5b645a5ea0b9a5e08a9
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-06-29 08:30:29 -0600

    Change bylaws restriction time to 14 days

--- contracts/eden/src/bylaws.cpp
+++ contracts/eden/src/bylaws.cpp
@@ -53,9 +53,9 @@ namespace eden
       if (next_state == ratified)
       {
          eosio::check(
-             pos->time().to_time_point() + eosio::days(90) <=
+             pos->time().to_time_point() + eosio::days(14) <=
                  state.last_election_time.to_time_point(),
-             "Bylaws can only be approved if they were proposed at least 90 days before the last "
+             "Bylaws can only be approved if they were proposed at least 14 days before the last "
              "election.");
       }
       if (pos->approvals().size() >= state.board.size() * 2 / 3)
